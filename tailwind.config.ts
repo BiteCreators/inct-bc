ec34b3d02b9655b4407da8546a114ce192275375
tapuru <pogladtapira@gmail.com>
2024-09-04 20:06:28 +0500

    fix: fix tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,14 +1,17 @@
 import type { Config } from 'tailwindcss'
-
 const config: Config = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './common/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/common/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   plugins: [],
   theme: {
     extend: {
+      backgroundImage: {
+        'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+      },
       colors: {
         accent: {
           100: '#73A5FF',
