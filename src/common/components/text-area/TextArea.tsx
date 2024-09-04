a6778d25cb9241912c8b03679367053a4447f884
tapuru <pogladtapira@gmail.com>
2024-09-04 20:19:16 +0500

    feat: add text area ui component

---
+++ src/common/components/text-area/TextArea.tsx
@@ -0,0 +1,82 @@
+import React, { ComponentProps, forwardRef } from 'react'
+
+import { cn } from '@/common/utils/cn'
+
+import { useTextArea } from './useTextArea'
+
+type Props = {
+  className?: string
+  error?: string
+  isError?: boolean
+  label?: string
+  resize?: 'auto' | 'manual-x' | 'manual-y'
+} & ComponentProps<'textarea'>
+
+export const TextArea = forwardRef<HTMLDivElement, Props>(
+  (
+    {
+      className,
+      disabled,
+      error,
+      id,
+      isError,
+      label,
+      onChange,
+      resize = 'auto',
+      value,
+      ...props
+    }: Props,
+    ref
+  ) => {
+    const { handleChange, textAreaRef } = useTextArea({ autoResize: resize === 'auto', onChange })
+
+    return (
+      <div className={'flex flex-col'} ref={ref}>
+        {!!label && (
+          <label
+            className={cn(['text-light-900  text-sm', disabled && 'text-dark-100'])}
+            htmlFor={id}
+          >
+            {label}
+          </label>
+        )}
+        <textarea
+          className={cn([
+            `
+          outline-none
+          px-3 
+          py-1.5 
+          text-light-100 
+          bg-dark-500 
+          border 
+          border-dark-100 
+          rounded-sm
+          focus:outline-accent-500
+          focus:border-none
+          active:border-light-100
+          active:border
+          disabled:text-dark-100
+          disabled:active:border-dark-100
+          placeholder:text-light-900
+          placeholder:text-sm
+          resize-none
+          outline-offset-0
+          overflow-y-hidden
+          min-h-8
+         `,
+            isError && 'border-danger-500',
+            resize === 'manual-y' && 'overflow-y-auto resize-y',
+            resize === 'manual-x' && 'overflow-x-auto resize-x',
+            className,
+          ])}
+          disabled={disabled}
+          id={id}
+          onChange={handleChange}
+          ref={textAreaRef}
+          {...props}
+        />
+        {isError && <p className={'text-danger-500 text-sm'}>{error}</p>}
+      </div>
+    )
+  }
+)
---
+++ src/common/components/text-area/useTextArea.ts
@@ -0,0 +1,32 @@
+import React, { useCallback, useEffect, useRef } from 'react'
+
+export const useTextArea = ({
+  autoResize,
+  onChange,
+}: {
+  autoResize?: boolean
+  onChange: React.ChangeEventHandler<HTMLTextAreaElement> | undefined
+}) => {
+  const textAreaRef = useRef<HTMLTextAreaElement>(null)
+
+  const calcHeight = useCallback(() => {
+    if (textAreaRef.current && autoResize) {
+      textAreaRef.current.style.height = 'inherit'
+      textAreaRef.current.style.height = `${textAreaRef.current.scrollHeight}px`
+    }
+  }, [textAreaRef, autoResize])
+
+  useEffect(() => {
+    calcHeight()
+  }, [textAreaRef, calcHeight])
+
+  const handleChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    calcHeight()
+    onChange?.(e)
+  }
+
+  return {
+    handleChange,
+    textAreaRef,
+  }
+}
