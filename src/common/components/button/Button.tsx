fe0939c573ecb607d3980c57febbb07d33dce6f2
Dmitriy <fstpain@gmail.com>
2024-09-07 03:08:04 +0300

    IN-12 fix: props

--- src/common/components/button/Button.tsx
+++ src/common/components/button/Button.tsx
@@ -11,11 +11,15 @@ export const Button = ({ className, variant = 'primary', ...props }: ButtonProps
     <button
       className={cn(
         'font-weight600 w-auto py-1.5 px-4 rounded-sm text-md text-slate-50 disabled:opacity-50 box-border focus:outline-primary-900 focus:outline-2 focus:outline',
-        variant === 'primary' && 'bg-primary-500 active:bg-primary-700 hover:bg-primary-100',
-        variant === 'secondary' && 'bg-dark-300 active:bg-dark-500 hover:bg-dark-100',
+        variant === 'primary' &&
+          'bg-primary-500 hover:bg-primary-700 active:bg-primary-900 disabled:bg-primary-500 disabled:hover:bg-primary-500',
+        variant === 'secondary' &&
+          'bg-dark-300 hover:bg-dark-500 active:bg-dark-600 disabled:bg-dark-300 disabled:hover:bg-dark-300',
         variant === 'outline' &&
-          'bg-none border-primary-500 border text-primary-500 active:border-primary-700 active:text-primary-700 hover:border-primary-100 hover:text-primary-100',
-        variant === 'text' && 'bg-none text-primary-500'
+          'bg-none border-primary-500 border text-primary-500 hover:border-primary-700 hover:text-primary-700 disabled:border-primary-500 disabled:text-primary-500 disabled:hover:border-primary-500 disabled:hover:text-primary-500', // Отключение ховера при disabled
+        variant === 'text' &&
+          'bg-none text-primary-500 hover:text-primary-700 disabled:text-primary-500 disabled:hover:text-primary-500',
+        className
       )}
       type={'button'}
       {...props}
