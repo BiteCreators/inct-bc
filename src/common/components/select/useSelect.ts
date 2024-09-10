78db28bd206902e9e382d62804016bafd6191344
tapuru <pogladtapira@gmail.com>
2024-09-10 15:16:33 +0500

    docs: add useSelect docs

--- src/common/components/select/useSelect.ts
+++ src/common/components/select/useSelect.ts
@@ -1,5 +1,10 @@
 import { useLayoutEffect, useRef, useState } from 'react'
 
+/**
+ * Sets content width to be equal to the width of the trigger button.
+ *
+ */
+
 export const useSelect = () => {
   const triggerRef = useRef<HTMLButtonElement>(null)
   const [contentWidth, setContentWidth] = useState(triggerRef.current?.clientWidth)
