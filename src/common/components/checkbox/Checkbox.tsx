8f44b8f2875fd57be67c783f2b92e1d3bb332e52
Julie <ulabalaeva07768@gmail.com>
2024-09-10 08:46:40 +0300

    fix IN-13: added Icon component

--- src/common/components/checkbox/Checkbox.tsx
+++ src/common/components/checkbox/Checkbox.tsx
@@ -2,6 +2,8 @@ import React, { ChangeEvent, ComponentProps, useId, useState } from 'react'
 
 import { cn } from '@/common/utils/cn'
 
+import { Icon } from '../icon/Icon'
+
 type CheckboxProps = {
   text?: string
 } & ComponentProps<'input'>
@@ -27,7 +29,7 @@ export const Checkbox = ({
     <div className={'inline-flex items-center'}>
       <input
         checked={isChecked}
-        className={'absolute z-(-1) opacity-0 '}
+        className={'absolute z-[-1] opacity-0'}
         disabled={disabled}
         id={id ?? checkboxId}
         onChange={handleChange}
@@ -39,30 +41,46 @@ export const Checkbox = ({
           `
           relative
           bg-inherit
-          w-8 h-8
+          w-9 h-9
           rounded-2xl
-          checked: 
-          active:bg-dark-100
-          hover:bg-dark-300
+          active:bg-dark-100 
+          hover:bg-dark-300 
           focus:bg-dark-500
+          disabled:bg-inherit 
+          disabled:hover:bg-inherit 
+          disabled:active:bg-inherit
           before:inline-block
           before:content-['']
           before:absolute
-          before:left-2
-          before:top-2
-          before:w-4
-          before:h-4
+          before:left-[9px]
+          before:top-[9px]
+          before:w-[18px]
+          before:h-[18px]
           before:border-solid
           before:border-2
           before:rounded-sm`,
-          isChecked && !disabled && 'before:bg-light-100 before:border-none ',
+          disabled && '!bg-inherit',
+          isChecked && !disabled && 'before:bg-light-100 before:border-none',
           isChecked && disabled && 'before:bg-dark-100 before:border-none',
           !isChecked && disabled && 'before:border-light-900',
           !isChecked && !disabled && 'before:border-light-500',
           className
         )}
         htmlFor={id ?? checkboxId}
-      ></label>
+      >
+        {isChecked && (
+          <Icon
+            className={cn(
+              'absolute left-[6px] top-[6px]',
+              disabled && `fill-current text-light-500`
+            )}
+            height={'24'}
+            iconId={'checkmark-outline'}
+            viewBox={'0 0 24 24'}
+            width={'24'}
+          />
+        )}
+      </label>
       {text && (
         <span className={cn('mx-1 font-weight400 text-sm', disabled && 'text-light-900')}>
           {text}
