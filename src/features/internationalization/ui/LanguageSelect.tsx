0d9105188ae688e2292b5a66f3321f57ff54297d
tapuru <pogladtapira@gmail.com>
2024-09-14 20:45:10 +0500

    feat: add basic i18n configuration

--- src/features/internationalization/ui/LanguageSelect.tsx
+++ src/features/internationalization/ui/LanguageSelect.tsx
@@ -2,26 +2,37 @@ import { useState } from 'react'
 
 import { Icon } from '@/common/components/icon/Icon'
 import { Select } from '@/common/components/select/Select'
+import { useTranslation } from '@/common/utils/hooks/useTranslation'
+import { useRouter } from 'next/router'
 
 export const LanguageSelect = () => {
-  const [lang, setLang] = useState<'en' | 'ru'>('ru')
+  const { asPath, defaultLocale, locale, locales, pathname, push, query } = useRouter()
+
+  const {
+    t: { Internationalization },
+  } = useTranslation()
+
+  const handleChange = (value: string) => {
+    push({ pathname, query }, asPath, { locale: value })
+  }
 
   return (
     <Select
+      className={'min-w-[163px]'}
+      defaultValue={defaultLocale}
       icon={
         <Icon
           height={'20px'}
-          iconId={lang === 'en' ? 'british-flag' : 'russian-flag'}
+          iconId={locale === 'en' ? 'british-flag' : 'russian-flag'}
           viewBox={'-3 -1.5 40 30'}
           width={'30px'}
         />
       }
-      onValueChange={(value: 'en' | 'ru') => setLang(value)}
-      value={lang}
-      width={'163px'}
+      onValueChange={handleChange}
+      value={locale}
     >
-      <Select.Item value={'ru'}>Russian</Select.Item>
-      <Select.Item value={'en'}>English</Select.Item>
+      <Select.Item value={'ru'}>{Internationalization.ru}</Select.Item>
+      <Select.Item value={'en'}>{Internationalization.en}</Select.Item>
     </Select>
   )
 }
