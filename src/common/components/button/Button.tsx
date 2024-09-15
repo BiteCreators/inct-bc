87f843a83c15cffe2757d018b4c4aaffec8fbe9b
Dmitriy <fstpain@gmail.com>
2024-09-15 15:56:24 +0300

    IN-35 fix: styles

--- src/common/components/button/Button.tsx
+++ src/common/components/button/Button.tsx
@@ -26,16 +26,22 @@ export const Button = ({
         'hover:bg-primary-700',
         'active:bg-primary-900',
         'disabled:bg-primary-500 disabled:hover:bg-primary-500',
-        variant === 'secondary' && 'bg-dark-300',
-        'hover:bg-dark-500',
-        'active:bg-dark-600',
-        'disabled:bg-dark-300 disabled:hover:bg-dark-300',
-        variant === 'outline' && 'bg-none border-primary-500 border text-primary-500',
-        'hover:border-primary-700 hover:text-primary-700 ',
-        'disabled:border-primary-500 disabled:text-primary-500 disabled:hover:border-primary-500 disabled:hover:text-primary-500', // Отключение ховера при disabled
-        variant === 'text' && 'bg-none text-primary-500',
-        ' hover:text-primary-700',
-        ' disabled:text-primary-500 disabled:hover:text-primary-500',
+        variant === 'secondary' && [
+          'bg-dark-300',
+          'hover:bg-dark-500',
+          'active:bg-dark-600',
+          'disabled:bg-dark-300 disabled:hover:bg-dark-300',
+        ],
+        variant === 'outline' && [
+          'bg-none border-primary-500 border text-primary-500',
+          'hover:border-primary-700 hover:text-primary-700 ',
+          'disabled:border-primary-500 disabled:text-primary-500 disabled:hover:border-primary-500 disabled:hover:text-primary-500',
+        ],
+        variant === 'text' && [
+          'bg-none text-primary-500',
+          ' hover:text-primary-700',
+          ' disabled:text-primary-500 disabled:hover:text-primary-500',
+        ],
         className
       )}
       type={'button'}
