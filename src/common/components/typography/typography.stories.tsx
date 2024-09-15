058739589cf354ec1d03525c0767e1e555b65f1f
Dmitriy <fstpain@gmail.com>
2024-09-15 13:34:45 +0300

    IN-35 fix : component props

--- src/common/components/typography/typography.stories.tsx
+++ src/common/components/typography/typography.stories.tsx
@@ -10,15 +10,21 @@ export default meta
 export const Typographys = {
   render: () => (
     <div className={'flex flex-col'}>
-      <Typography variant={'h2'}>Heading 1</Typography>
-      <Typography variant={'h3'}>Heading 2</Typography>
-      <Typography variant={'h3'}>Heading 2</Typography>
-      <Typography variant={'bold_text_14'}>Heading 2</Typography>
-      <Typography variant={'large'}>Heading 2</Typography>
-      <Typography variant={'regular_text_16'}>Heading 2</Typography>
-      <Typography variant={'small_link'}>Heading 2</Typography>
-      <Typography variant={'semi_bold_small_text'}>Heading 2</Typography>
-      <Typography variant={'small_text'}>Heading 2</Typography>
+      <Typography variant={'h2'}>Lorem ipsum dolor sit amet, consectetur adipisicing.</Typography>
+      <Typography variant={'h3'}>Lorem ipsum dolor sit amet, consectetur adipisicing.</Typography>
+      <Typography variant={'h3'}>Lorem ipsum dolor sit amet, consectetur adipisicing.</Typography>
+      <Typography variant={'large'}>
+        Lorem ipsum dolor sit amet, consectetur adipisicing.
+      </Typography>
+      <Typography variant={'regular-text'}>
+        Lorem ipsum dolor sit amet, consectetur adipisicing.
+      </Typography>
+      <Typography variant={'regular-link'}>
+        Lorem ipsum dolor sit amet, consectetur adipisicing.
+      </Typography>
+      <Typography className={'font-bold'} variant={'small-text'}>
+        Lorem ipsum dolor sit amet, consectetur adipisicing.
+      </Typography>
     </div>
   ),
 }
