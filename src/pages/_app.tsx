5f49477e8f5c62df60290d930dad37e161986f50
tapuru <pogladtapira@gmail.com>
2024-09-17 15:26:39 +0500

    IN-32 feat: add default layout

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,35 @@
 import type { AppProps } from 'next/app'
 
+import React from 'react'
+
+import { PageLayout } from '@/common/components/page-layout/PageLayout'
+import { Header } from '@/widgets/header'
+import { NextPage } from 'next'
+
 import '@/styles/globals.css'
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+export type NextPageWithLayout<P = {}, IP = P> = {
+  getLayout?: (page: React.ReactElement) => React.ReactNode
+} & NextPage<P, IP>
+
+type AppPropsWithLayout = {
+  Component: NextPageWithLayout
+} & AppProps
+
+const DefaultLayout = (page: React.ReactElement) => {
+  return (
+    <PageLayout
+      footer={<div>footer test</div>}
+      header={<Header />}
+      sidebar={<div>sidebar test</div>}
+    >
+      {page}
+    </PageLayout>
+  )
+}
+
+export default function App({ Component, pageProps }: AppPropsWithLayout) {
+  const getLayout = Component.getLayout ?? DefaultLayout
+
+  return getLayout(<Component {...pageProps} />)
 }
