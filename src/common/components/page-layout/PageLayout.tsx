53df70f24e402884b87f4d498e092b620bad0286
tapuru <pogladtapira@gmail.com>
2024-09-17 21:14:49 +0500

    IN-32 feat: add custom scroll to page layout

--- src/common/components/page-layout/PageLayout.tsx
+++ src/common/components/page-layout/PageLayout.tsx
@@ -2,6 +2,8 @@ import React from 'react'
 
 import { cn } from '@/common/utils/cn'
 
+import { ScrollArea } from '../scroll/ScrollArea'
+
 type Props = {
   children?: React.ReactNode
   footer?: React.ReactNode
@@ -12,13 +14,15 @@ type Props = {
 
 export const PageLayout = ({ children, footer, header, mainClassName, sidebar }: Props) => {
   return (
-    <div className={cn('flex flex-col min-h-[100vh]')}>
+    <div className={cn('min-h-[100vh]')}>
       {!!header && <div className={'sticky top-0'}>{header}</div>}
-      <div className={'flex-grow grid grid-cols-[220px_1fr]'}>
-        {!!sidebar && <div className={'sticky top-[60px] left-0 self-start'}>{sidebar}</div>}
-        <main className={cn('mt-9', mainClassName)}>{children}</main>
-      </div>
-      {!!footer && <div>{footer}</div>}
+      <ScrollArea className={'h-[calc(100vh-60px)] flex flex-col'}>
+        <div className={'flex-grow grid grid-cols-[220px_1fr]'}>
+          {!!sidebar && <div className={'sticky top-0 left-0 self-start'}>{sidebar}</div>}
+          <main className={cn('mt-9', mainClassName)}>{children}</main>
+        </div>
+        {!!footer && <div>{footer}</div>}
+      </ScrollArea>
     </div>
   )
 }
