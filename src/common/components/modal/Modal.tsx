3f3e3666b529dda5f3110523626b25bf948e7e88
fireonex <mari.krulikovskaya@mail.ru>
2024-09-18 16:37:38 +0300

    IN-28 feat: add ModalContent

--- src/common/components/modal/Modal.tsx
+++ src/common/components/modal/Modal.tsx
@@ -1,6 +1,7 @@
 import { ReactNode } from 'react'
 
 import { Close } from '@/common/assets/icons/components'
+import { cn } from '@/common/utils/cn'
 import * as Dialog from '@radix-ui/react-dialog'
 
 type Props = {
@@ -13,21 +14,21 @@ type Props = {
 export const Modal = ({ children, isOpen, onOpenChange, title }: Props) => {
   return (
     <Dialog.Root onOpenChange={onOpenChange} open={isOpen}>
-      <Dialog.Overlay className={'fixed inset-0 bg-black/50'} />
+      <Dialog.Overlay className={cn('fixed inset-0 bg-black/50')} />
       <Dialog.Content
-        className={
-          'fixed top-1/2 left-1/2 max-w-md bg-dark-300 rounded-sm transform border border-dark-100' +
-          ' -translate-x-1/2 -translate-y-1/2'
-        }
+        className={cn(
+          'fixed top-1/2 left-1/2 max-w-md bg-dark-300 rounded-sm transform border border-dark-100',
+          '-translate-x-1/2 -translate-y-1/2'
+        )}
       >
-        <div className={'flex justify-between items-center py-3 px-6'}>
-          <Dialog.Title className={'text-xl font-bold'}>{title}</Dialog.Title>
-          <Dialog.Close className={'focus:outline-none cursor-pointer'}>
-            <Close className={'fill-current text-light-100'} />
+        <div className={cn('flex justify-between items-center py-3 px-6')}>
+          <Dialog.Title className={cn('text-xl font-bold')}>{title}</Dialog.Title>
+          <Dialog.Close className={cn('focus:outline-none cursor-pointer')}>
+            <Close className={cn('fill-current text-light-100')} />
           </Dialog.Close>
         </div>
-        <div className={'h-px bg-dark-100 w-full'}></div>
-        <div className={'py-3 px-6'}>{children}</div>
+        <div className={cn('h-px bg-dark-100 w-full')} />
+        <div className={cn('py-3 px-6')}>{children}</div>
       </Dialog.Content>
     </Dialog.Root>
   )
---
+++ src/common/components/modal/ModalContent.tsx
@@ -0,0 +1,10 @@
+import { ReactNode } from 'react'
+
+type Props = {
+  children: ReactNode
+  title?: string
+}
+
+export const ModalContent = ({ children, title }: Props) => {
+  return <></>
+}
