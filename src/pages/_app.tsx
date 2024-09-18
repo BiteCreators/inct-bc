81c4e0acfd0210a199d95ce413361ff9905c9bb9
Dmitriy <fstpain@gmail.com>
2024-09-18 23:54:37 +0300

    IN-31 init: rtk

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,18 @@
 import type { AppProps } from 'next/app'
 
+import React from 'react'
+import { Provider } from 'react-redux'
+
+import { wrapper } from '@/app/store'
+
 import '@/styles/globals.css'
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+export default function App({ Component, ...rest }: AppProps) {
+  const { props, store } = wrapper.useWrappedStore(rest)
+
+  return (
+    <Provider store={store}>
+      <Component {...props.pageProps} />
+    </Provider>
+  )
 }
