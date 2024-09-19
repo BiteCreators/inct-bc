cc11d2fc4d2e81be661a140a67b9429993bd5224
DeBook <depacos1@gmail.com>
2024-09-19 13:12:23 +0300

    added form

--- src/pages/auth/sign-in/SingIn.tsx
+++ src/pages/auth/sign-in/SingIn.tsx
@@ -1,5 +1,24 @@
 import React from 'react'
 
-export default function SingIn() {
-  return <h1>SignIn</h1>
+import { PageLayout } from '@/common/components/page-layout/PageLayout'
+import { SignInForm } from '@/features/auth/ui/SignInForm'
+import { NextPageWithLayout } from '@/pages/_app'
+import { Header } from '@/widgets/header'
+
+const SignIn: NextPageWithLayout = () => {
+  return (
+    <div className={'flex gap-4 flex-col justify-center w-full min-w-[290px] max-w-[380px]'}>
+      <SignInForm />
+    </div>
+  )
 }
+
+SignIn.getLayout = (page: React.ReactElement) => {
+  return (
+    <PageLayout header={<Header />} mainClassName={'flex flex-col items-center justify-center '}>
+      {page}
+    </PageLayout>
+  )
+}
+
+export default SignIn
