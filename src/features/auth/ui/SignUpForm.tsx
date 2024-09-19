35765bc4632d431880ebbfeed390ca894de10855
tapuru <pogladtapira@gmail.com>
2024-09-20 00:11:41 +0500

    IN-44 feat: add errors translations to sign up form

--- src/features/auth/ui/SignUpForm.tsx
+++ src/features/auth/ui/SignUpForm.tsx
@@ -13,7 +13,11 @@ import { SignUpFormData, signUpSchema } from '../lib/schemas/signUp.schema'
 import { SignInButton } from './SignInButton'
 
 export const SignUpForm = () => {
-  const { control, handleSubmit } = useForm<SignUpFormData>({
+  const {
+    control,
+    formState: { errors },
+    handleSubmit,
+  } = useForm<SignUpFormData>({
     defaultValues: {
       agreedToPrivacyPolicy: false,
       email: '',
@@ -42,10 +46,23 @@ export const SignUpForm = () => {
         {t.signUp}
       </Typography>
       <form className={'flex flex-col gap-6'} noValidate onSubmit={handleSubmit(submit)}>
-        <FormInput control={control} label={t.username} name={'username'} required />
-        <FormInput control={control} label={t.email} name={'email'} required />
         <FormInput
           control={control}
+          error={t[errors?.username?.message as keyof typeof t]}
+          label={t.username}
+          name={'username'}
+          required
+        />
+        <FormInput
+          control={control}
+          error={t[errors?.email?.message as keyof typeof t]}
+          label={t.email}
+          name={'email'}
+          required
+        />
+        <FormInput
+          control={control}
+          error={t[errors?.password?.message as keyof typeof t]}
           inputType={'reveal'}
           label={t.password}
           name={'password'}
@@ -53,6 +70,7 @@ export const SignUpForm = () => {
         />
         <FormInput
           control={control}
+          error={t[errors?.passwordConfirmation?.message as keyof typeof t]}
           inputType={'reveal'}
           label={t.passwordConfirmation}
           name={'passwordConfirmation'}
@@ -60,6 +78,7 @@ export const SignUpForm = () => {
         />
         <FormCheckbox
           control={control}
+          error={t[errors?.agreedToPrivacyPolicy?.message as keyof typeof t]}
           name={'agreedToPrivacyPolicy'}
           required
           //TODO: rewrite with updated checkbox
