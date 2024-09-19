10358be3e0458cb3e1643531667b6a8c6eb0c811
tapuru <pogladtapira@gmail.com>
2024-09-20 00:10:33 +0500

    IN-44 feat: add auth errors consts and fix sign up schema

--- src/features/auth/lib/schemas/signUp.schema.ts
+++ src/features/auth/lib/schemas/signUp.schema.ts
@@ -1,20 +1,37 @@
 import { z } from 'zod'
 
+import { AUTH_ERRORS } from '../consts'
+
 export const signUpSchema = z
   .object({
-    agreedToPrivacyPolicy: z.boolean({ required_error: 'privacyPolicyRequiredError' }),
-    email: z.string().email().min(1, 'email is required'),
+    agreedToPrivacyPolicy: z.boolean({ required_error: AUTH_ERRORS.PRIVACY_POLICY_REQUIRED_ERROR }),
+    email: z
+      .string()
+      .email({ message: AUTH_ERRORS.EMAIL_INVALID_ERROR })
+      .min(1, AUTH_ERRORS.EMAIL_REQUIRED_ERROR),
     password: z
       .string()
-      .min(1, 'passwordRequiredError')
-      .min(6, 'password can not be less than 6 symbols'),
+      .min(1, AUTH_ERRORS.PASSWORD_REQUIRED_ERROR)
+      .max(30, AUTH_ERRORS.PASSWORD_TOO_LONG_ERROR)
+      .min(6, AUTH_ERRORS.PASSWORD_TOO_SHORT_ERROR)
+      .regex(
+        new RegExp(/^[a-zA-Z0-9!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~]+$/g),
+        AUTH_ERRORS.PASSWORD_INVALID_ERROR
+      ),
     passwordConfirmation: z
       .string()
-      .min(1, 'password confirmation is required')
-      .min(6, 'password can not be less than 6 symbols'),
-    username: z.string().min(1, 'username is required'),
+      .min(1, AUTH_ERRORS.PASSOWRD_CONFIRMATION_REQUIRED_ERROR)
+      .min(6, AUTH_ERRORS.PASSWORD_TOO_SHORT_ERROR),
+    username: z
+      .string()
+      .min(1, AUTH_ERRORS.USERNAME_REQUIRED_ERROR)
+      .min(6, AUTH_ERRORS.USERNAME_TOO_SHORT_ERROR)
+      .max(30, AUTH_ERRORS.USERNAME_TOO_LONG_ERROR),
   })
-  .refine(data => data.password === data.passwordConfirmation)
+  .refine(data => data.password === data.passwordConfirmation, {
+    message: AUTH_ERRORS.PASSOWRD_CONFIRMATION_INVALID_ERROR,
+    path: ['passwordConfirmation'],
+  })
 
 export type SignUpFormData = z.infer<typeof signUpSchema>
 
