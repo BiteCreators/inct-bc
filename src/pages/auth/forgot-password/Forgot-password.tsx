c5a41676579864f78b17aa3e4bdafca484c489c7
Dmitriy <fstpain@gmail.com>
2024-09-20 23:23:03 +0300

    IN-40: feat : Forgot

---
+++ src/pages/auth/forgot-password/Forgot-password.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+import { AuthLayout } from '@/app/layouts/auth-layout'
+import { NextPageWithLayout } from '@/pages/_app'
+
+const ForgotPassword: NextPageWithLayout = () => {
+  return <div>GGGG</div>
+}
+
+ForgotPassword.getLayout = AuthLayout
+
+export default ForgotPassword
