8174c0ddb567f9bea7491c457b83c7b8cc7038d8
Dmitriy <fstpain@gmail.com>
2024-09-20 23:23:56 +0300

    IN-40: refactor : other component

--- src/pages/auth/sign-in/SingIn.tsx
+++ src/pages/auth/sign-in/SingIn.tsx
@@ -1,5 +1,12 @@
 import React from 'react'
 
+import Link from 'next/link'
+
 export default function SingIn() {
-  return <h1>SignIn</h1>
+  return (
+    <>
+      <h1>SignIn</h1>
+      <Link href={'/auth/forgot-password'}>Forgot Password</Link>
+    </>
+  )
 }
