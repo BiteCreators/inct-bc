93d9d1b39919dfc57e8ba98ae40c2f0f1b72ea98
tapuru <pogladtapira@gmail.com>
2024-09-22 11:17:35 +0500

    IN-51 refactor: move base api and redux hooks to common folder

--- src/common/api/inct.api.ts
+++ src/common/api/inct.api.ts
@@ -6,16 +6,6 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 // Define a service using a base URL and expected endpoints
 export const inctagramApi = createApi({
   baseQuery: fetchBaseQuery({ baseUrl: 'https://inctagram.work/api/' }),
-  endpoints: builder => ({
-    getAllPostsTest: builder.query<Post, void>({
-      query: () => {
-        return { url: `/v1/public-posts/all/` }
-      },
-    }),
-  }),
+  endpoints: () => ({}),
   reducerPath: 'inctagramApi',
 })
-
-// Export hooks for usage in functional components, which are
-// auto-generated based on the defined endpoints
-export const { useGetAllPostsTestQuery } = inctagramApi
