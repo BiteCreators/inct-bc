b91ea6c0ec6ba6889c8b1492c773d377595bd0d3
tapuru <pogladtapira@gmail.com>
2024-09-22 13:10:33 +0500

    IN-51 feat: add auth api

---
+++ src/common/api/auth.api.ts
@@ -0,0 +1,20 @@
+import { inctagramApi } from './inct.api'
+
+type RegistrationDto = {
+  baseUrl: string
+  email: string
+  password: string
+  userName: string
+}
+
+export const authApi = inctagramApi.injectEndpoints({
+  endpoints: builder => ({
+    registration: builder.mutation<void, RegistrationDto>({
+      query: body => ({
+        body,
+        method: 'POST',
+        url: '/auth/registration',
+      }),
+    }),
+  }),
+})
--- src/common/api/inct.api.ts
+++ src/common/api/inct.api.ts
@@ -1,11 +1,10 @@
 // Need to use the React-specific entry point to import createApi
 
-import { Post } from '@/app/inct.types'
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
 // Define a service using a base URL and expected endpoints
 export const inctagramApi = createApi({
-  baseQuery: fetchBaseQuery({ baseUrl: 'https://inctagram.work/api/' }),
+  baseQuery: fetchBaseQuery({ baseUrl: 'https://inctagram.work/api/v1' }),
   endpoints: () => ({}),
   reducerPath: 'inctagramApi',
 })
