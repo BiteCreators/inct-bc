b07c5a5b74577ce7695f7baccaf588d135df145c
tapuru <pogladtapira@gmail.com>
2024-09-26 23:19:53 +0500

    IN-75 refactor: refactor layouts (move default layout, add auth layout where needed)

--- src/pages/auth/sign-up/SignUp.tsx
+++ src/pages/auth/sign-up/SignUp.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-import { AuthLayout } from '@/app/layouts/auth-layout'
+import { AuthLayout } from '@/app/layouts/AuthLayout'
 import { SignUpForm } from '@/features/auth'
 import { NextPageWithLayout } from '@/pages/_app'
 
