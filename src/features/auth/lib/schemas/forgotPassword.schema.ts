ba43b1d199898ea1e3e1e0c03aad35d9d857761b
tapuru <pogladtapira@gmail.com>
2024-09-27 14:05:54 +0500

    IN-75 refactor: fix bad types and schemas naming in auth feature

--- src/features/auth/lib/schemas/forgotPassword.schema.ts
+++ src/features/auth/lib/schemas/forgotPassword.schema.ts
@@ -1,11 +1,11 @@
 import { LocaleType } from '@/locales/en'
 import { z } from 'zod'
 
-export const createForgotPasswordScheme = (t: LocaleType['Auth']) =>
+export const createForgotPasswordSchema = (t: LocaleType['Auth']) =>
   z.object({
     baseUrl: z.string(),
     email: z.string().email().min(1, t.emailRequiredError),
     recaptcha: z.string().min(1, t.recaptcha),
   })
 
-export type forgotPasswordData = z.infer<ReturnType<typeof createForgotPasswordScheme>>
+export type ForgotPasswordFormData = z.infer<ReturnType<typeof createForgotPasswordSchema>>
