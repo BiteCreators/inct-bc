3419875362b90800a7cc0ebffd014286de9a21ad
tapuru <pogladtapira@gmail.com>
2024-09-28 15:16:38 +0500

    IN-62 feat: add mobeleAppMenu

--- src/features/navigation/index.ts
+++ src/features/navigation/index.ts
@@ -1,3 +1,4 @@
 export { AppLogo } from './ui/AppLogo'
 export { HeaderMenu } from './ui/HeaderMenu'
+export { MobileAppMenu } from './ui/MobileAppMenu'
 export { SideNavbar } from './ui/SideNavbar'
