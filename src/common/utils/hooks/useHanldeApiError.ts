8df735e271b5d76a04d1839a0b3c801f30e7fc9d
tapuru <pogladtapira@gmail.com>
2024-10-02 19:10:26 +0500

    IN-84 feat: add network error handling

--- src/common/utils/hooks/useHanldeApiError.ts
+++ src/common/utils/hooks/useHanldeApiError.ts
@@ -1,11 +1,14 @@
 import React from 'react'
 import { FieldValues, Path, UseFormSetError } from 'react-hook-form'
 
+import { LocaleType } from '@/locales/en'
+
 import { isApiError, isApiErrorWithArrary, isFetchBaseQueryError } from '../api-helpers'
 import { useScopedTranslation } from './useTranslation'
 
-export const useHandleApiErorr = () => {
-  const t = useScopedTranslation('Common')
+export const useHandleApiErorr = <NT extends keyof LocaleType>(namespace: NT) => {
+  const internalT = useScopedTranslation('Common')
+  const t = useScopedTranslation(namespace)
 
   const handleApiError = <T extends FieldValues>({
     error,
@@ -14,13 +17,21 @@ export const useHandleApiErorr = () => {
     setError,
   }: {
     error: unknown
-    modifyMessage?: (message: string) => { field?: Path<T>; message: string }
+    modifyMessage?: (
+      message: string,
+      t: LocaleType[typeof namespace]
+    ) => { field?: Path<T>; message: string }
     setApiError: React.Dispatch<React.SetStateAction<string>>
     setError?: UseFormSetError<T>
   }) => {
     if (isFetchBaseQueryError(error)) {
+      if (error.status === 'FETCH_ERROR') {
+        setApiError(internalT.networkError)
+
+        return
+      }
       if (error.status === 500) {
-        setApiError(t.internalServerError)
+        setApiError(internalT.internalServerError)
 
         return
       }
@@ -28,7 +39,7 @@ export const useHandleApiErorr = () => {
     if (isApiErrorWithArrary(error)) {
       error.data.messages.forEach(m => {
         const { field, message } = modifyMessage
-          ? modifyMessage(m.message)
+          ? modifyMessage(m.message, t)
           : { field: m.field as Path<T>, message: m.message }
 
         setError
@@ -40,7 +51,7 @@ export const useHandleApiErorr = () => {
     }
     if (isApiError(error)) {
       const { field, message } = modifyMessage
-        ? modifyMessage(error.data.messages)
+        ? modifyMessage(error.data.messages, t)
         : { field: undefined, message: error.data.messages }
 
       setError && field ? setError(field, { message, type: 'server' }) : setApiError(message)
