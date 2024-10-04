0089e5a88c8603e4f4133fd1dd0ea2b421e5bda7
Dmitriy <fstpain@gmail.com>
2024-10-05 00:43:59 +0300

    in-56: feat: redirect

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,44 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
+import { authApi } from '@/common/api/auth.api'
+import { useAppDispatch } from '@/common/lib/hooks/reduxHooks'
+import { Button } from '@/common/ui'
+import { authSlice } from '@/features/auth/model/auth.slice'
 import { Inter } from 'next/font/google'
 import Link from 'next/link'
+import { useSearchParams } from 'next/navigation'
+import Router, { useRouter } from 'next/router'
 
 const inter = Inter({ subsets: ['latin'] })
 
 const Home = () => {
+  const searchParams = useSearchParams()
+  const validationCode = searchParams?.get('code') ?? null
+  const router = useRouter()
+  const dispatch = useAppDispatch()
+
+  const [googleAuth] = authApi.useGoogleAuthMutation()
+  const [meResponse, { data }] = authApi.useLazyMeQuery()
+  const handler = async () => {
+    if (validationCode) {
+      const { accessToken: token } = await googleAuth({ code: validationCode }).unwrap()
+
+      console.log(data)
+      await meResponse()
+
+      document.cookie = `accessToken=${token};max-age=3600;secure;path=/;samesite=strict`
+      // dispatch(authSlice.actions.setAccessToken(token))
+      Router.push(`/profile${data?.userId}`)
+    }
+  }
+  const handlerz = () => {
+    console.log(data)
+  }
+
   return (
     <div className={'flex gap-4 flex-col'}>
+      <Button onClick={handler}>btn</Button>
+      <Button onClick={handlerz}>ME</Button>
       <h1>StartPage</h1>
       <Link className={'text-red-600 text-4xl'} href={'/auth'}>
         Auth
@@ -16,22 +47,30 @@ const Home = () => {
       <div className={'text-[50px]'}>
         Aegon Targārio Ānogār Luqisūti Daor rhaenagon iā tubis jēdi. Yn ziry iāndes korona iā
         Qēlioroz brōzi kesrio tolī ēza lēda hāre ossȳngas, se vēzos tubis luqagon daor ēza
-        mazvēlzir, ēdrus Dorne kostōro ezīmagon. Qilūbri gevives luqagon lo va doron ēza iā Āegon
-        issa ziry tolmon issa ābra. Luqagon hen mazves lo azantys valzir, ēdrus lo ēza daor bē
-        ipradagon emāqi ivēza, tolmon. Ziry ēza daor ūndan emaragon ziry ēza zōbrar. Rybis zīrī
-        dāezma dorinagon ēza Āegono ēdrus tolī lo ēzīmagon, vēzos rūvario sēn emagon. Lo ziry ēza
-        daor. Tolī ēza mazvēlzir ēza Āegono īssāt. Sūlos mazves lo ēza udagon iā Korona īlva Rōzda,
-        ēzi ēza jāton iā zābravo ēza lentos mērios ziry issa rūs emāzmo. Valyria jemēlos hen lentys
-        ēza vēzos pojaliks zōbrar. Skōra zolūpty jelmio ānogri ēzos belmur mōrī īlva iemnōti, va
-        naejot ēza bē, iēdro ēza dorinazmor. Targārio luoty ēza daor ēnykso lo sūndas tolmiot, se
-        rybis belmoti mazvēlzir ēza zūgagon Aenaro bē Dāreksio Vezof tōna va jāre. Yn tolvys ēza
-        daor. Tāba rūho Dēnys iā rōvalāksōna ēza jāton, se ēzos ōrēssys Māzis va Vestrasī,
-        vāedarzies zōbrar. Hen ābrazma tolvūpas ēza darilar hen sȳndror nykeēza. Dāreksio Vezof mēre
-        vēzos bē tubis ānogrīs ēza biarzes dorios hen Valyria. Ziry ēza syt pōjes hen Iynkē Skōriot
-        lo ēza rytsos hen Lōgrion. Targārio tolminar iā vēzos lo ēza rīhiltary Velarion hen
-        Driftmark (mēre vēzos zōbrī Valyria) ēza pāplan, rūñagon iā daorār hārezī kīdaks. Velarion
-        iā kēly ānogri zōbrī Celtigar hen Kogarō Ānogrī ēza bantar iā sīmon Uzvestrī morghon hen
-        Sūdy ēza Targārio drakari ēdrus iā sōvior.
+        mazvēlzir, ēdrus Dorne kostōro ezīmagon. Любимая фишка многих цыган — развод честных людей
+        на деньги средь бела дня. Многие цыганки хорошо владеют практиками гипноза, поэтому одного
+        простого зрительного контакта с ними достаточно для того, чтобы начать слушать и выполнять
+        все то, что они скажут. Также часто взрослые цыганки ходят в сопровождении ребятишек,
+        которые не просто поздно шатаются рядом с мамой, а выполняют свою незатейливую работу по
+        опустошению карманов жертвы. Пока мама заговаривает жертве зубы и отвлекает внимание
+        мимолетным гипнозом — ребятишки проверяют содержимое карманов на предмет наличия ценных
+        вещей, если таковые попадаются то самым невозбранным образом отправляются во владение
+        предприимчивой цыганской семейки. Qilūbri gevives luqagon lo va doron ēza iā Āegon issa ziry
+        tolmon issa ābra. Luqagon hen mazves lo azantys valzir, ēdrus lo ēza daor bē ipradagon emāqi
+        ivēza, tolmon. Ziry ēza daor ūndan emaragon ziry ēza zōbrar. Rybis zīrī dāezma dorinagon ēza
+        Āegono ēdrus tolī lo ēzīmagon, vēzos rūvario sēn emagon. Lo ziry ēza daor. Tolī ēza
+        mazvēlzir ēza Āegono īssāt. Sūlos mazves lo ēza udagon iā Korona īlva Rōzda, ēzi ēza jāton
+        iā zābravo ēza lentos mērios ziry issa rūs emāzmo. Valyria jemēlos hen lentys ēza vēzos
+        pojaliks zōbrar. Skōra zolūpty jelmio ānogri ēzos belmur mōrī īlva iemnōti, va naejot ēza
+        bē, iēdro ēza dorinazmor. Targārio luoty ēza daor ēnykso lo sūndas tolmiot, se rybis belmoti
+        mazvēlzir ēza zūgagon Aenaro bē Dāreksio Vezof tōna va jāre. Yn tolvys ēza daor. Tāba rūho
+        Dēnys iā rōvalāksōna ēza jāton, se ēzos ōrēssys Māzis va Vestrasī, vāedarzies zōbrar. Hen
+        ābrazma tolvūpas ēza darilar hen sȳndror nykeēza. Dāreksio Vezof mēre vēzos bē tubis ānogrīs
+        ēza biarzes dorios hen Valyria. Ziry ēza syt pōjes hen Iynkē Skōriot lo ēza rytsos hen
+        Lōgrion. Targārio tolminar iā vēzos lo ēza rīhiltary Velarion hen Driftmark (mēre vēzos
+        zōbrī Valyria) ēza pāplan, rūñagon iā daorār hārezī kīdaks. Velarion iā kēly ānogri zōbrī
+        Celtigar hen Kogarō Ānogrī ēza bantar iā sīmon Uzvestrī morghon hen Sūdy ēza Targārio
+        drakari ēdrus iā sōvior.
       </div>
     </div>
   )
