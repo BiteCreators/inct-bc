3817206458118206587e8e9061cbc916b65f4782
Dmitriy <fstpain@gmail.com>
2024-10-06 15:30:57 +0300

    in-90: fix: checkbox\datepicker hover on mobile device

--- src/common/ui/datepicker/CustomDayPickerStyles.ts
+++ src/common/ui/datepicker/CustomDayPickerStyles.ts
@@ -1,7 +1,7 @@
-const hoverOutsideRange = ' hover:bg-primary-700 '
+const hoverOutsideRange = ' global-hover:hover:bg-primary-700 '
 const navButtonStyle =
   'w-9 h-9 flex items-center justify-center rounded-full transition-all duration-100' +
-  ' ease-in-out hover:w-9 hover:h-9 hover:bg-dark-100'
+  ' ease-in-out global-hover:hover:w-9 global-hover:hover:h-9 global-hover:hover:bg-dark-100'
 
 export const DayPickerStyle = {
   active: 'bg-primary-900 w-9 h-9 flex items-center justify-center',
@@ -9,9 +9,9 @@ export const DayPickerStyle = {
   button_previous: navButtonStyle,
   caption: 'text-white text-center font-semibold mb-10',
   caption_label: 'font-inter text-md font-bold text-left absolute top-[22px] ml-2',
-  chevron: 'fill-white hover:bg-dark-100 rounded-full w-5 h-5',
+  chevron: 'fill-white global-hover:hover:bg-dark-100 rounded-full w-5 h-5',
   day:
-    'hover:rounded-full relative w-9 h-9 flex items-center justify-center' +
+    'global-hover:hover:rounded-full relative w-9 h-9 flex items-center justify-center' +
     ' cursor-pointer focus:outline-none focus-visible:ring-2 focus-visible:ring-primary-300' +
     ' focus-visible:ring-offset-0' +
     hoverOutsideRange,
