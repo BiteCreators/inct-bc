8576a374f9277d4ef040e16aa0a2ca5ca20290f7
fireonex <mari.krulikovskaya@mail.ru>
2024-10-08 15:12:04 +0300

    IN-110 init: start header-dropdown task

--- src/features/navigation/ui/HeaderMenu.tsx
+++ src/features/navigation/ui/HeaderMenu.tsx
@@ -1,10 +1,36 @@
-import { MoreHorizontal } from '@/common/assets/icons/components'
+import { SettingsOutline } from '@/common/assets/icons/components'
+import { Dropdown } from '@/common/ui'
+import { DropdownItem } from '@/common/ui/dropdown/Dropdown'
 
 export const HeaderMenu = () => {
-  //TODO: add dropdown
+  const items: DropdownItem[] = [
+    {
+      icon: <SettingsOutline />,
+      label: 'Profile Settings',
+      onClick: () => {},
+    },
+  ]
+  // items: [
+  //   {
+  //     icon: <Person />,
+  //     label: 'Profile Settings',
+  //     onClick: () => alert('Profile Settings clicked'),
+  //   },
+  //   {
+  //     icon: <PaperPlane />,
+  //     label: 'Statistics',
+  //     onClick: () => alert('Statistics clicked'),
+  //   },
+  //   { label: 'Favorites', onClick: () => alert('Favorites clicked') },
+  //   { label: 'Log Out', onClick: () => alert('Log Out clicked') },
+  // ],
+
   return (
-    <button onClick={() => alert("this doesn't do anything yet")}>
-      <MoreHorizontal />
-    </button>
+    <>
+      <Dropdown className={'ml-6 -mt-0.5'} items={items}></Dropdown>
+    </>
+    // <button onClick={() => alert("this doesn't do anything yet")}>
+    //   <MoreHorizontal />
+    // </button>
   )
 }
