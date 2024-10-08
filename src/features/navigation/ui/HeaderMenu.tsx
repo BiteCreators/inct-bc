009c38ee8546fd6fcbc1f40cd8b23e5e0476c7e6
fireonex <mari.krulikovskaya@mail.ru>
2024-10-08 17:33:24 +0300

    IN-110 feat: add navigation in dropdown in header

--- src/features/navigation/ui/HeaderMenu.tsx
+++ src/features/navigation/ui/HeaderMenu.tsx
@@ -1,36 +1,100 @@
-import { SettingsOutline } from '@/common/assets/icons/components'
+import { authApi } from '@/common/api/auth.api'
+import {
+  BookmarkOutline,
+  LogOut,
+  PersonOutline,
+  PlusSquareOutline,
+  SettingsOutline,
+  TrendingUp,
+} from '@/common/assets/icons/components'
+import { useAppDispatch, useAppSelector } from '@/common/lib/hooks/reduxHooks'
+import { useScopedTranslation } from '@/common/lib/hooks/useTranslation'
 import { Dropdown } from '@/common/ui'
 import { DropdownItem } from '@/common/ui/dropdown/Dropdown'
+import { authSlice } from '@/features/auth/model/auth.slice'
+import { useRouter } from 'next/router'
 
 export const HeaderMenu = () => {
-  const items: DropdownItem[] = [
+  const router = useRouter()
+  const isAuth = useAppSelector(authSlice.selectors.selectAccessToken)
+  const tAuth = useScopedTranslation('Auth')
+  const tNav = useScopedTranslation('Navigation')
+
+  const dispatch = useAppDispatch()
+  const [logout] = authApi.useLogoutMutation()
+
+  if (router.pathname.startsWith('/auth') && router.pathname !== '/auth') {
+    return null
+  }
+
+  const handleLogout = async () => {
+    try {
+      await logout()
+        .unwrap()
+        .then(() => {
+          document.cookie = `accessToken=;expires=${new Date(0)}`
+          dispatch(authSlice.actions.setAccessToken(null))
+        })
+    } catch (err) {
+      console.log('Logout error', err)
+    }
+  }
+
+  const loggedInItems: DropdownItem[] = [
     {
       icon: <SettingsOutline />,
-      label: 'Profile Settings',
-      onClick: () => {},
+      label: tNav.profileSettings,
+      onClick: async () => {
+        await router.push('/profile-settings')
+      },
+    },
+    {
+      icon: <TrendingUp />,
+      label: tNav.statistics,
+      onClick: async () => {
+        await router.push('/statistics')
+      },
+    },
+    {
+      icon: <BookmarkOutline />,
+      label: tNav.favorites,
+      onClick: async () => {
+        await router.push('/favorites')
+      },
+    },
+    {
+      icon: <LogOut />,
+      label: tAuth.logOut,
+      onClick: handleLogout,
     },
   ]
-  // items: [
-  //   {
-  //     icon: <Person />,
-  //     label: 'Profile Settings',
-  //     onClick: () => alert('Profile Settings clicked'),
-  //   },
-  //   {
-  //     icon: <PaperPlane />,
-  //     label: 'Statistics',
-  //     onClick: () => alert('Statistics clicked'),
-  //   },
-  //   { label: 'Favorites', onClick: () => alert('Favorites clicked') },
-  //   { label: 'Log Out', onClick: () => alert('Log Out clicked') },
-  // ],
 
-  return (
-    <>
-      <Dropdown className={'ml-6 -mt-0.5'} items={items}></Dropdown>
-    </>
-    // <button onClick={() => alert("this doesn't do anything yet")}>
-    //   <MoreHorizontal />
-    // </button>
-  )
+  const loggedOutItems: DropdownItem[] = [
+    {
+      icon: <PersonOutline />,
+      label: tAuth.signIn,
+      onClick: async () => {
+        try {
+          await router.push('/auth/sign-in')
+        } catch (err) {
+          console.log('Sign In error', err)
+        }
+      },
+    },
+    {
+      icon: <PlusSquareOutline />,
+      label: tAuth.signUp,
+      onClick: async () => {
+        try {
+          await router.push('/auth/sign-up')
+        } catch (err) {
+          console.log('Sign Up error', err)
+        }
+      },
+    },
+  ]
+
+  const items = isAuth ? loggedInItems : loggedOutItems
+
+  return <Dropdown className={'ml-6 -mt-0.5'} items={items} />
 }
