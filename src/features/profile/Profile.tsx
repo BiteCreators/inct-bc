4a2d860eec7e4f0e38c6edec218c86a0be7703b0
Dmitriy <fstpain@gmail.com>
2024-10-09 17:41:23 +0300

    in-104: test

---
+++ src/features/profile/Profile.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import { Avatar } from '@/features/profile/avatar/Avatar'
+
+export const Profile = () => {
+  return (
+    <div className={'bg-danger-100 w-full h-[80vh] flex'}>
+      <Avatar></Avatar>
+    </div>
+  )
+}
--- src/pages/profile/[id].tsx
+++ src/pages/profile/[id].tsx
@@ -1,10 +1,15 @@
 import React from 'react'
 
+import { Profile } from '@/features/profile/Profile'
 import { useRouter } from 'next/router'
 
 export default function CurrentProfile() {
   const router = useRouter()
   const { id } = router.query
 
-  return <div>Profile ID : {id}</div>
+  return (
+    <div>
+      <Profile />
+    </div>
+  )
 }
