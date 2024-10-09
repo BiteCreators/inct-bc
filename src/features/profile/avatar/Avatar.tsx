9d275f5bf9346869871450f9d207f6e95cd37883
Dmitriy <fstpain@gmail.com>
2024-10-09 12:44:31 +0300

    in-104: add: modal window

--- src/features/profile/avatar/Avatar.tsx
+++ src/features/profile/avatar/Avatar.tsx
@@ -1,10 +1,11 @@
-import React, { useRef } from 'react'
+import React, { useRef, useState } from 'react'
 
 import Image from '@/common/assets/icons/components/Image'
-import { Button, Input } from '@/common/ui'
+import { Button, Input, Modal, Typography } from '@/common/ui'
 
 export const Avatar = () => {
   const fileInputRef = useRef<HTMLInputElement | null>(null)
+  const [isOpen, setIsOpen] = useState(false)
 
   const UploadImage = () => {
     if (fileInputRef.current) {
@@ -14,15 +15,29 @@ export const Avatar = () => {
 
   return (
     <div className={'bg-dark-700 w-1/5 flex flex-col items-center gap-5 p-2'}>
-      <div
+      <Image
         className={'bg-dark-500 flex w-full aspect-square rounded-full justify-center items-center'}
       >
         <Image />
-      </div>
+      </Image>
       <Button className={'w-full'} onClick={UploadImage} variant={'outline'}>
         add
       </Button>
+      <Button
+        onClick={() => {
+          setIsOpen(!isOpen)
+        }}
+      >
+        Open modal
+      </Button>
       <input accept={'.jpg, .jpeg, .png'} className={'hidden'} ref={fileInputRef} type={'file'} />
+      {isOpen && (
+        <Modal isOpen={isOpen} mode={'default'} onOpenChange={setIsOpen} title={'AVATAR CROP'}>
+          <div className={'flex flex-col gap-[18px] pb-6 pt-[18px] w-80'}>
+            <Button>Crop image</Button>
+          </div>
+        </Modal>
+      )}
     </div>
   )
 }
