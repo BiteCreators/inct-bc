a0f2001e11d31777820ddfef15a57b5cae7f4ab9
Dmitriy <fstpain@gmail.com>
2024-10-09 15:05:35 +0300

    in-104: feat: crop window

--- src/features/profile/avatar/Avatar.tsx
+++ src/features/profile/avatar/Avatar.tsx
@@ -1,41 +1,104 @@
 import React, { useRef, useState } from 'react'
+import ReactCrop, { Crop, centerCrop, makeAspectCrop } from 'react-image-crop'
 
-import Image from '@/common/assets/icons/components/Image'
-import { Button, Input, Modal, Typography } from '@/common/ui'
+import { Button, Modal } from '@/common/ui'
+
+import 'react-image-crop/dist/ReactCrop.css'
 
 export const Avatar = () => {
   const fileInputRef = useRef<HTMLInputElement | null>(null)
   const [isOpen, setIsOpen] = useState(false)
+  const [imageUrl, setImageUrl] = useState<null | string>(null)
+
+  const [crop, setCrop] = useState<Crop>({
+    height: 50,
+    unit: '%',
+    width: 50,
+    x: 25,
+    y: 25,
+  })
+
+  const onSelectFile = (e: any) => {
+    const file = e.target.files?.[0]
+
+    if (!file) {
+      return
+    }
+    const reader = new FileReader()
+
+    reader.addEventListener('load', () => {
+      const url = reader.result?.toString() || ''
+
+      setImageUrl(url)
+      setIsOpen(true)
+
+      console.log(imageUrl)
+    })
+    reader.readAsDataURL(file)
+  }
 
   const UploadImage = () => {
     if (fileInputRef.current) {
       fileInputRef.current.click()
     }
   }
+  const onImageLoad = (e: any) => {
+    const { height, width } = e.currentTarget
+
+    const crop = centerCrop(
+      makeAspectCrop(
+        {
+          unit: 'px',
+          width: 200,
+        },
+        1,
+        width,
+        height
+      ),
+      width,
+      height
+    )
+    const centeredCrop = centerCrop(crop, width, height)
+
+    setCrop(centeredCrop)
+  }
 
   return (
     <div className={'bg-dark-700 w-1/5 flex flex-col items-center gap-5 p-2'}>
-      <Image
-        className={'bg-dark-500 flex w-full aspect-square rounded-full justify-center items-center'}
-      >
-        <Image />
-      </Image>
       <Button className={'w-full'} onClick={UploadImage} variant={'outline'}>
         add
       </Button>
-      <Button
-        onClick={() => {
-          setIsOpen(!isOpen)
-        }}
-      >
-        Open modal
-      </Button>
-      <input accept={'.jpg, .jpeg, .png'} className={'hidden'} ref={fileInputRef} type={'file'} />
+      <input
+        accept={'.jpg, .jpeg, .png'}
+        className={'hidden'}
+        onChange={onSelectFile}
+        ref={fileInputRef}
+        type={'file'}
+      />
       {isOpen && (
-        <Modal isOpen={isOpen} mode={'default'} onOpenChange={setIsOpen} title={'AVATAR CROP'}>
-          <div className={'flex flex-col gap-[18px] pb-6 pt-[18px] w-80'}>
-            <Button>Crop image</Button>
-          </div>
+        <Modal
+          className={'max-w-[492px]'}
+          isOpen={isOpen}
+          mode={'default'}
+          onOpenChange={setIsOpen}
+          title={'Add profile photo'}
+        >
+          {imageUrl && (
+            <div className={'w-[444px] flex flex-col items-end'}>
+              <ReactCrop
+                aspect={1}
+                circularCrop
+                className={'w-full'}
+                crop={crop}
+                keepSelection
+                minWidth={200}
+                onChange={c => setCrop(c)}
+              >
+                <img alt={'Selected'} className={'w-full'} onLoad={onImageLoad} src={imageUrl} />
+              </ReactCrop>
+              <Button className={'mt-6 mb-4 px-7'}>Save</Button>
+            </div>
+          )}
         </Modal>
       )}
     </div>
