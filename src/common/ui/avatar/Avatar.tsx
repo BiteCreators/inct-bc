b3559798953637eea9db7be754f6db815b938e41
Dmitriy <fstpain@gmail.com>
2024-10-10 14:47:56 +0300

    in-104: add: close variant

--- src/common/ui/avatar/Avatar.tsx
+++ src/common/ui/avatar/Avatar.tsx
@@ -3,6 +3,8 @@ import Link from 'next/link'
 type Props = {
   alt?: string
   avatarURL: string
+  close?: boolean
+  closeFunc?: () => void
   href?: string
   isNextLink?: boolean
   rel?: string
@@ -12,18 +14,35 @@ type Props = {
 export const Avatar = ({
   alt = 'Avatar',
   avatarURL,
+  close = false,
+  closeFunc,
   href,
   isNextLink = false,
   rel = '',
   size,
 }: Props) => {
+  const closeHandler = () => {
+    if (closeFunc) {
+      closeFunc
+    }
+  }
   const AvatarImage = (
-    <img
-      alt={alt}
-      className={'rounded-full object-cover'}
-      src={avatarURL}
-      style={{ height: `${size}px`, width: `${size}px` }}
-    />
+    <div className={`relative w-[${size}px h-[${size}px rounded-full`}>
+      <img
+        alt={alt}
+        className={'rounded-full object-cover'}
+        src={avatarURL}
+        style={{ height: `${size}px`, width: `${size}px` }}
+      />
+      {close && (
+        <div
+          className={
+            'bg-danger-100 absolute rounded-full w-8 h-8 right-0 top-1/4 transform -translate-y-1/2'
+          }
+          onClick={closeHandler}
+        ></div>
+      )}
+    </div>
   )
 
   return isNextLink && href ? (
