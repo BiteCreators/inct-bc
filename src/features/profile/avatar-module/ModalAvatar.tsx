1a88c244b3ba240a8dee55033980c7c73c49ad94
Dmitriy <fstpain@gmail.com>
2024-10-10 16:38:14 +0300

    in-104: ref

--- src/features/profile/avatar-module/ModalAvatar.tsx
+++ src/features/profile/avatar-module/ModalAvatar.tsx
@@ -1,8 +1,9 @@
 import React, { useRef, useState } from 'react'
-import { Crop, ReactCrop, centerCrop, convertToPixelCrop, makeAspectCrop } from 'react-image-crop'
+import { Crop, convertToPixelCrop } from 'react-image-crop'
 
 import { Button, Modal } from '@/common/ui'
 import setCanvasPreview from '@/common/ui/avatar/setCanvasPreview'
+import { CropImage } from '@/features/profile/avatar-module/CropImage'
 
 type Props = {
   isOpen: boolean
@@ -11,7 +12,6 @@ type Props = {
 }
 
 export const ModalAvatar = ({ isOpen, setIsOpen, updateAvatar }: Props) => {
-  const imgRef = useRef<HTMLImageElement | null>(null)
   const previewImgRef = useRef<HTMLCanvasElement | null>(null)
   const [error, setError] = useState('')
   const fileInputRef = useRef<HTMLInputElement | null>(null)
@@ -24,32 +24,12 @@ export const ModalAvatar = ({ isOpen, setIsOpen, updateAvatar }: Props) => {
     y: 25,
   })
 
-  const onImageLoad = (e: any) => {
-    const { height, width } = e.currentTarget
-    const crop = centerCrop(
-      makeAspectCrop(
-        {
-          unit: 'px',
-          width: 150,
-        },
-        1,
-        width,
-        height
-      ),
-      width,
-      height
-    )
-    const centeredCrop = centerCrop(crop, width, height)
-
-    setCrop(centeredCrop)
-  }
-
   const UploadImage = () => {
     if (fileInputRef.current) {
       fileInputRef.current.click()
     }
   }
-  const onSelectFile = (e: any) => {
+  const onSelectFile = (e: React.ChangeEvent<HTMLInputElement>) => {
     const file = e.target.files?.[0]
 
     if (!file) {
@@ -80,14 +60,15 @@ export const ModalAvatar = ({ isOpen, setIsOpen, updateAvatar }: Props) => {
     })
     reader.readAsDataURL(file)
   }
-  const saveCroppedImage = () => {
-    if (imgRef.current && previewImgRef.current) {
-      const pixelCrop = convertToPixelCrop(crop, imgRef.current.width, imgRef.current.height)
+  const saveCroppedImage = (imgRef: HTMLImageElement | null) => {
+    if (imgRef && previewImgRef.current) {
+      const pixelCrop = convertToPixelCrop(crop, imgRef.width, imgRef.height)
 
-      setCanvasPreview(imgRef.current, previewImgRef.current, pixelCrop)
+      setCanvasPreview(imgRef, previewImgRef.current, pixelCrop)
     }
     const dataUrl = previewImgRef.current?.toDataURL()
 
+    //updateAvatar(dataUrl), setIsOpen(false) закомментить что бы видеть превью аватара
     if (dataUrl) {
       updateAvatar(dataUrl)
     }
@@ -106,29 +87,12 @@ export const ModalAvatar = ({ isOpen, setIsOpen, updateAvatar }: Props) => {
       {error}
       <div className={'w-full items-end'}>
         {imageUrl && (
-          <div className={'w-[444px] flex flex-col items-end'}>
-            <ReactCrop
-              aspect={1}
-              circularCrop
-              className={'w-full flex overflow-auto max-h-[60vh]'}
-              crop={crop}
-              keepSelection
-              minHeight={150}
-              minWidth={150}
-              onChange={(crop, percentageCrop) => setCrop(percentageCrop)}
-            >
-              <img
-                alt={'Selected'}
-                className={''}
-                onLoad={onImageLoad}
-                ref={imgRef}
-                src={imageUrl}
-              />
-            </ReactCrop>
-            <Button className={'mt-6 mb-4 px-7'} onClick={saveCroppedImage}>
-              Save
-            </Button>
-          </div>
+          <CropImage
+            crop={crop}
+            imageUrl={imageUrl}
+            saveCroppedImage={saveCroppedImage}
+            setCrop={setCrop}
+          />
         )}
         {!imageUrl && (
           <div className={'w-full flex flex-col justify-center items-center gap-5'}>
@@ -146,7 +110,13 @@ export const ModalAvatar = ({ isOpen, setIsOpen, updateAvatar }: Props) => {
         ref={fileInputRef}
         type={'file'}
       />
-      {crop && <canvas className={'border object-contain w-36 h-36'} ref={previewImgRef} />}
+      {crop && (
+        //hidden - позволяет отобразить или скрыть превью
+        <canvas
+          className={'border object-contain w-[150px] hidden h-[150px]'}
+          ref={previewImgRef}
+        />
+      )}
     </Modal>
   )
 }
