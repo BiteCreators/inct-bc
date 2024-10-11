30f4b231c2df40cac72875bd791a2601f00506b0
tapuru <pogladtapira@gmail.com>
2024-10-11 23:14:29 +0500

    IN-122 feat: add session card

--- src/features/devices/ui/SessionCard.tsx
+++ src/features/devices/ui/SessionCard.tsx
@@ -1,3 +1,43 @@
-export const SessionCard = () => {
-  return <div></div>
+import React from 'react'
+
+import {
+  Brave,
+  BrowserPlaceholder,
+  ChromeSvgrepoCom,
+  Explorer,
+  Firefox,
+  MicrosoftEdge,
+  Opera,
+  Safari,
+  UcBrowser,
+  Yandex,
+} from '@/common/assets/icons/components'
+import { Card, Typography } from '@/common/ui'
+
+import { useSessionCard } from '../model/useSessionCard'
+
+type Props = {
+  action?: React.ReactNode
+  browserName: string
+  ip: string
+  lastVisit?: string
+  type: 'browser' | 'device'
+}
+
+export const SessionCard = ({ action, browserName, ip, lastVisit, type }: Props) => {
+  const { icon, lastVisitDate, title } = useSessionCard({ browserName, lastVisit, type })
+
+  return (
+    <Card className={'flex p-6 pt-[18px] justify-between'}>
+      <div className={'flex gap-3'}>
+        <div className={'mt-[6px]'}>{icon}</div>
+        <div>
+          <Typography variant={'h2'}>{title}</Typography>
+          <Typography className={'mt-3'}>IP: {ip}</Typography>
+          {!!lastVisit && <Typography className={'mt-3'}>Last visit: {lastVisitDate}</Typography>}
+        </div>
+      </div>
+      {!!action && <div>{action}</div>}
+    </Card>
+  )
 }
