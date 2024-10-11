7b9f65531d0af80b8e6641cab96e189511a2f045
tapuru <pogladtapira@gmail.com>
2024-10-11 23:15:59 +0500

    IN-122 feat: add Sessions list

--- src/features/devices/ui/SessionsList.tsx
+++ src/features/devices/ui/SessionsList.tsx
@@ -1,5 +1,36 @@
+import { devicesApi } from '@/common/api/devices.api'
+import { Typography } from '@/common/ui'
+
 import { SessionCard } from './SessionCard'
 
 export const SessionsList = () => {
-  return <SessionCard />
+  const { data, error, isError, isLoading, isSuccess } = devicesApi.useGetSessionsQuery()
+
+  if (isSuccess) {
+    const { current, others } = data
+
+    return (
+      <div>
+        <Typography className={'mb-[6px]'} variant={'h2'}>
+          Active sessions
+        </Typography>
+
+        {others.map(session => {
+          if (session.deviceId === current.deviceId) {
+            return null
+          }
+
+          return (
+            <SessionCard
+              browserName={session.browserName}
+              ip={session.ip}
+              key={session.deviceId}
+              lastVisit={session.lastActive}
+              type={'browser'}
+            />
+          )
+        })}
+      </div>
+    )
+  }
 }
