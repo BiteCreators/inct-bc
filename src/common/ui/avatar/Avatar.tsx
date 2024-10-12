75dceb528b704bb61f477e5de3f1ab0640a9696e
Dmitriy <fstpain@gmail.com>
2024-10-12 17:07:17 +0300

    in-104: feat:  cross for closs

--- src/common/ui/avatar/Avatar.tsx
+++ src/common/ui/avatar/Avatar.tsx
@@ -1,11 +1,15 @@
+import { cn } from '@/common/lib/utils/cn'
 import Link from 'next/link'
 
 type Props = {
   alt?: string
-  avatarURL?: string
-  href: string
-  isNextLink: boolean
+  avatarURL: string
+  href?: string
+  isNextLink?: boolean
+  onClose?: () => void
   rel?: string
+  rounded?: boolean
+  showClose?: boolean
   size: number
 }
 
@@ -14,19 +18,38 @@ export const Avatar = ({
   avatarURL,
   href,
   isNextLink = false,
+  onClose,
   rel = '',
+  rounded = true,
+  showClose = false,
   size,
 }: Props) => {
+  const closeHandler = () => {
+    if (onClose) {
+      onClose()
+    }
+  }
   const AvatarImage = (
-    <img
-      alt={alt}
-      className={'rounded-full object-cover'}
-      src={avatarURL}
-      style={{ height: `${size}px`, width: `${size}px` }}
-    />
+    <div className={cn([`relative w-[${size}px h-[${size}px`])}>
+      <img
+        alt={alt}
+        className={cn([rounded && 'rounded-full', 'object-cover'])}
+        src={avatarURL}
+        style={{ height: `${size}px`, width: `${size}px` }}
+      />
+      {showClose && (
+        <div
+          className={cn([
+            'bg-danger-100 absolute w-8 h-8 right-0 top-1/4 transform -translate-y-1/2',
+            rounded && 'rounded-full',
+          ])}
+          onClick={closeHandler}
+        ></div>
+      )}
+    </div>
   )
 
-  return isNextLink ? (
+  return isNextLink && href ? (
     <Link href={href} rel={rel}>
       {AvatarImage}
     </Link>
