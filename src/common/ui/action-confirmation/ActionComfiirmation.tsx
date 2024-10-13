40ef07036fec20a4434f5f0da6d522053ffdbe78
Dmitriy <fstpain@gmail.com>
2024-10-13 22:28:38 +0300

    in-102: feat:  action confirm component

--- src/common/ui/action-confirmation/ActionComfiirmation.tsx
+++ src/common/ui/action-confirmation/ActionComfiirmation.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import { cn } from '@/common/lib/utils/cn'
 import { Button, Modal } from '@/common/ui'
@@ -6,25 +6,38 @@ import { Button, Modal } from '@/common/ui'
 type Props = {
   isOpen: boolean
   message: string
+  onConfirm: () => void
+  onReject: () => void
+  setIsOpen: (isOpen: boolean) => void
   title: string
 }
 
 export const ActionConfirmation = ({
   isOpen = false,
-  message = 'test message with questiont 2 line i hope test message with questiont 2 line i hope',
+  message = 'test message with question',
+  onConfirm,
+  onReject,
+  setIsOpen,
   title = 'test Title',
 }: Props) => {
-  const confirm = () => {}
-  const reject = () => {}
+  const handleConfirm = () => {
+    onConfirm()
+    setIsOpen(false)
+  }
+
+  const handleReject = () => {
+    onReject()
+    setIsOpen(false)
+  }
 
   return (
-    <Modal isOpen={isOpen} mode={'default'} title={title}>
+    <Modal isOpen={isOpen} mode={'default'} onOpenChange={setIsOpen} title={title}>
       <div className={'calc(min-w-[430px]-40px) flex mb-4'}>{message}</div>
       <div className={'w-full flex gap-5 mb-6 items-center justify-end'}>
-        <Button className={'h-[37px]'} onClick={confirm} variant={'outline'}>
+        <Button className={'h-[37px]'} onClick={handleConfirm} variant={'outline'}>
           Да
         </Button>
-        <Button>Нет</Button>
+        <Button onClick={handleReject}>Нет</Button>
       </div>
     </Modal>
   )
