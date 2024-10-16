0cec4105767ab1838ed7c38ce2e8739c2aa6fb7d
DePacos <depacos@yandex.ru>
2024-10-16 14:46:09 +0300

    IN-119-fix-width

--- src/pages/profile/[id]/settings/index.tsx
+++ src/pages/profile/[id]/settings/index.tsx
@@ -2,7 +2,7 @@ import { ProfileSettingsTabs } from '@/widgets/profile-settings-tabs'
 
 export default function ProfileSettings() {
   return (
-    <div className={'px-4 lg:pl-6 lg:pr-16 md:w-full'}>
+    <div className={'px-4 lg:pl-6 lg:pr-16 w-screen md:w-full'}>
       <ProfileSettingsTabs />
     </div>
   )
