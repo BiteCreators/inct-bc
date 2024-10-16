4290502ceb8abd0da545375a552ada04457d7734
DmitryChues <dmitrychues2312@gmail.com>
2024-10-16 12:16:29 +0300

    IN-121: remove index

--- src/locales/en.ts
+++ src/locales/en.ts
@@ -1,13 +1,11 @@
-import {
-  authTranslationEn,
-  commonTranslationEn,
-  devicesTranslationsEn,
-  internationalizationTranslationEn,
-  navigationTranslationEn,
-  privacyPolicyTranslationEn,
-  profileTranslationEn,
-  termsServiceTranslationEn,
-} from '@/locales'
+import { authTranslationEn } from '@/locales/auth'
+import { commonTranslationEn } from '@/locales/common'
+import { devicesTranslationsEn } from '@/locales/devices'
+import { internationalizationTranslationEn } from '@/locales/internationalization'
+import { navigationTranslationEn } from '@/locales/navigation'
+import { privacyPolicyTranslationEn } from '@/locales/privacy-policy'
+import { profileTranslationEn } from '@/locales/profile'
+import { termsServiceTranslationEn } from '@/locales/terms-service'
 
 export const en = {
   Auth: authTranslationEn,
