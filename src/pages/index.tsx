51f0e2da31153349baaeae6dfb4f517ca1d91e4a
tapuru <pogladtapira@gmail.com>
2024-10-20 23:19:12 +0500

    IN-144 feat: add main page posts

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,40 +1,43 @@
 import React from 'react'
 
-import { Inter } from 'next/font/google'
-import Link from 'next/link'
+import { AuthLayout } from '@/app/layouts/AuthLayout'
+import { Post } from '@/entities/posts'
+import { PublicPostCard } from '@/features/posts'
+import { GetServerSideProps, InferGetServerSidePropsType } from 'next'
 
-const inter = Inter({ subsets: ['latin'] })
+import { NextPageWithLayout } from './_app'
 
-const Home = () => {
+type PublicPostsResponse = {
+  items: Post[]
+  pageSize: number
+  totalCount: number
+}
+
+export const getServerSideProps = (async () => {
+  const res = await fetch(
+    `${process.env.NEXT_PUBLIC_API_URL}/v1/public-posts/all/,?sortDirection=desc&pageSize=4`
+  )
+  const postsData: PublicPostsResponse = await res.json()
+
+  return { props: { postsData } }
+}) satisfies GetServerSideProps<{ postsData: PublicPostsResponse }>
+
+const Main: NextPageWithLayout<{ postsData: PublicPostsResponse }> = ({
+  postsData,
+}: InferGetServerSidePropsType<typeof getServerSideProps>) => {
+  const { items: posts } = postsData
+
   return (
-    <div className={'flex gap-4 flex-col'}>
-      <h1>StartPage</h1>
-      <Link className={'text-red-600 text-4xl'} href={'/auth'}>
-        Auth
-      </Link>
-      <Link href={'/profile'}>Profile</Link>
-      <div className={'text-[50px]'}>
-        Aegon Targārio Ānogār Luqisūti Daor rhaenagon iā tubis jēdi. Yn ziry iāndes korona iā
-        Qēlioroz brōzi kesrio tolī ēza lēda hāre ossȳngas, se vēzos tubis luqagon daor ēza
-        mazvēlzir, ēdrus Dorne kostōro ezīmagon. Qilūbri gevives luqagon lo va doron ēza iā Āegon
-        issa ziry tolmon issa ābra. Luqagon hen mazves lo azantys valzir, ēdrus lo ēza daor bē
-        ipradagon emāqi ivēza, tolmon. Ziry ēza daor ūndan emaragon ziry ēza zōbrar. Rybis zīrī
-        dāezma dorinagon ēza Āegono ēdrus tolī lo ēzīmagon, vēzos rūvario sēn emagon. Lo ziry ēza
-        daor. Tolī ēza mazvēlzir ēza Āegono īssāt. Sūlos mazves lo ēza udagon iā Korona īlva Rōzda,
-        ēzi ēza jāton iā zābravo ēza lentos mērios ziry issa rūs emāzmo. Valyria jemēlos hen lentys
-        ēza vēzos pojaliks zōbrar. Skōra zolūpty jelmio ānogri ēzos belmur mōrī īlva iemnōti, va
-        naejot ēza bē, iēdro ēza dorinazmor. Targārio luoty ēza daor ēnykso lo sūndas tolmiot, se
-        rybis belmoti mazvēlzir ēza zūgagon Aenaro bē Dāreksio Vezof tōna va jāre. Yn tolvys ēza
-        daor. Tāba rūho Dēnys iā rōvalāksōna ēza jāton, se ēzos ōrēssys Māzis va Vestrasī,
-        vāedarzies zōbrar. Hen ābrazma tolvūpas ēza darilar hen sȳndror nykeēza. Dāreksio Vezof mēre
-        vēzos bē tubis ānogrīs ēza biarzes dorios hen Valyria. Ziry ēza syt pōjes hen Iynkē Skōriot
-        lo ēza rytsos hen Lōgrion. Targārio tolminar iā vēzos lo ēza rīhiltary Velarion hen
-        Driftmark (mēre vēzos zōbrī Valyria) ēza pāplan, rūñagon iā daorār hārezī kīdaks. Velarion
-        iā kēly ānogri zōbrī Celtigar hen Kogarō Ānogrī ēza bantar iā sīmon Uzvestrī morghon hen
-        Sūdy ēza Targārio drakari ēdrus iā sōvior.
+    <div className={'max-w-[972px] pt-6 mx-auto'}>
+      <div className={'grid grid-cols-4 gap-3'}>
+        {posts.map(post => (
+          <PublicPostCard key={post.id} post={post} />
+        ))}
       </div>
     </div>
   )
 }
 
-export default Home
+Main.getLayout = AuthLayout
+
+export default Main
