df791d3b3e4515f70578a562e2e2a0a890e91cd5
Dmitriy <fstpain@gmail.com>
2024-10-20 13:16:16 +0300

    IN-138: feat: edit post

--- src/features/edit-post/EditPost.tsx
+++ src/features/edit-post/EditPost.tsx
@@ -1,17 +1,35 @@
-import React from 'react'
+import React, { ChangeEvent, useState } from 'react'
 
-import { Modal } from '@/common/ui'
+import { Avatar, Button, Modal, TextArea, Typography } from '@/common/ui'
 import Image from 'next/image'
 
 type Props = {
+  changeOpen: (e: boolean) => void
   isOpen: boolean
+  postText?: string
+  urlProfile?: string
 }
-export default function EditPost(props: Props) {
+export default function EditPost({
+  changeOpen,
+  isOpen,
+  postText = 'text text text',
+  urlProfile = 'default url',
+}: Props) {
+  const [textAreaText, setTextAreaText] = useState<string>(postText)
+  const changeText = (e: ChangeEvent<HTMLTextAreaElement>) => {
+    setTextAreaText(postText)
+  }
+  const saveChanges = () => {
+    // textAreaText=>save
+    changeOpen(false)
+  }
+
   return (
     <Modal
       className={'h-[560px] max-w-[970px] '}
-      isOpen={props.isOpen}
+      isOpen={isOpen}
       mode={'default'}
+      onOpenChange={changeOpen}
       title={'Edit post'}
     >
       <div className={'w-[920px] h-[465px] flex flex-row'}>
@@ -22,7 +40,21 @@ export default function EditPost(props: Props) {
             src={'https://cs14.pikabu.ru/post_img/big/2023/02/13/8/1676295806139337963.png'}
           />
         </div>
-        <div className={'w-1/2 h-full bg-blue-300'}></div>
+        <div className={'w-1/2 h-full pl-4'}>
+          <div className={'flex w-full justify-start items-center gap-5 mb-6'}>
+            <Avatar
+              avatarURL={'https://cs14.pikabu.ru/post_img/big/2023/02/13/8/1676295806139337963.png'}
+              imgStyles={'w-9 h-9'}
+            />
+            <Typography variant={'medium-text'}>{urlProfile}</Typography>
+          </div>
+          <TextArea label={'Add publication descriptions'} onChange={changeText}>
+            {textAreaText}
+          </TextArea>
+          <Button className={'absolute bottom-10 right-6'} onClick={saveChanges}>
+            Save changes
+          </Button>
+        </div>
       </div>
     </Modal>
   )
