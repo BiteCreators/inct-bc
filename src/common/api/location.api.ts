5163c8e0fc398e36cf32fe7eb2f9b939288e3c6d
DePacos <depacos@yandex.ru>
2024-10-24 17:10:32 +0300

    refactor code

--- src/common/api/location.api.ts
+++ src/common/api/location.api.ts
@@ -1,6 +1,6 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
-type CountryResponse = {
+export type CountryResponse = {
   continentCode: string
   countryCode: string
   division1Code: string
