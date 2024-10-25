29ea6189b4304bd26d81559182f9336956141e57
Dmitriy <fstpain@gmail.com>
2024-10-25 20:25:07 +0300

    IN-138: ref: RegisteredUsers.tsx

--- src/widgets/registered-users/RegisteredUsers.tsx
+++ src/widgets/registered-users/RegisteredUsers.tsx
@@ -6,25 +6,33 @@ type Props = {
   usersCount: number
 }
 export const RegisteredUsers = ({ usersCount }: Props) => {
-  const x = usersCount.toString().padStart(6, '0').split('')
-  const Palka = <div className={'w-[1px] h-1/2 bg-light-900'}></div>
-  const count = x.map((value, index) => {
-    if (index < x.length - 1) {
+  const usersCountArray = usersCount.toString().padStart(6, '0').split('')
+  const verticalBar = <div className={'w-[1px] h-1/2 bg-dark-100'}></div>
+  const usersForDisplay = usersCountArray.map((value, index) => {
+    if (index < usersCountArray.length - 1) {
       return (
         <React.Fragment key={index}>
-          <Typography variant={'text-'}>{value}</Typography>
-          {Palka}
+          <Typography className={'text-lg font-bold'}>{value}</Typography>
+          {verticalBar}
         </React.Fragment>
       )
     } else {
-      return <Typography key={index}>{value}</Typography>
+      return (
+        <Typography className={'text-lg font-bold'} key={index}>
+          {value}
+        </Typography>
+      )
     }
   })
 
   return (
-    <div className={'max-w-[972px] w-full h-16 bg-dark-500 px-7 flex justify-between items-center'}>
+    <div
+      className={'max-w-[972px] w-full h-[72px] bg-dark-500 px-7 flex justify-between items-center'}
+    >
       <Typography variant={'medium-text'}>Registered users:</Typography>
-      <div className={' border h-2/3 w-auto px-5 bg-dark-700 flex items-center gap-2'}>{count}</div>
+      <div className={' border h-2/3 w-auto px-5 bg-dark-700 flex items-center gap-2'}>
+        {usersForDisplay}
+      </div>
     </div>
   )
 }
