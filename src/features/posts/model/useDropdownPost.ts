0720657fa38df7eb2af49e2faa9300b6c076a096
Dmitriy <fstpain@gmail.com>
2024-10-29 19:19:31 +0300

    IN-138: ref: redirect

--- src/features/posts/model/useDropdownPost.ts
+++ src/features/posts/model/useDropdownPost.ts
@@ -1,22 +1,25 @@
 import { postsApi } from '@/common/api/posts.api'
 import { useConfirmation } from '@/common/ui/action-confirmation/useConfirmation'
 import { useParams } from 'next/navigation'
+import { useRouter } from 'next/router'
 
 export const useDropdownPost = () => {
   const { confirmOpen, handleConfirm, handleReject, requestConfirmation, setConfirmOpen } =
     useConfirmation()
+  const router = useRouter()
   const copyLinkHandler = async () => {
     await navigator.clipboard.writeText(window.location.href)
   }
   const [deletePost] = postsApi.useDeletePostMutation()
   const params = useParams()
-  const postId = Number(params?.id) ?? null
+  const postId = Number(params?.postId) ?? null
 
   const deletePostHandler = async () => {
     const isConfirmed = await requestConfirmation()
 
     if (isConfirmed) {
-      deletePost({ postId })
+      await deletePost({ postId })
+      await router.push('/')
     }
   }
 
