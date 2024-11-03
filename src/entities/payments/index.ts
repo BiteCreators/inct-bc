64ca2728977a1690ff0f6bd0a36f982756fa7daf
Dmitriy <fstpain@gmail.com>
2024-11-03 15:06:16 +0300

    IN-193: ref: refactor index reExport

--- src/entities/payments/index.ts
+++ src/entities/payments/index.ts
@@ -1,7 +1,2 @@
 export { paymentsApi } from '@/entities/payments/api/payments.api'
-export type {
-  CostPaymentResponse,
-  CurrentPaymentResponse,
-  SubscriptionData,
-  SubscriptionsRequest,
-} from '@/entities/payments/types/payments.type'
+export type { CostPayment, MyPayment, Subscription } from '@/entities/payments/types/payments.type'
