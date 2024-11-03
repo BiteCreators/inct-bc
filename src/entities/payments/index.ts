6e3dbdb7caf4cf72188e05a74abf8feaf8fb3d9e
Dmitriy <fstpain@gmail.com>
2024-11-03 13:13:19 +0300

    IN-193: feat: reExport

---
+++ src/entities/payments/index.ts
@@ -0,0 +1,7 @@
+export { paymentsApi } from '@/entities/payments/api/payments.api'
+export type {
+  CostPaymentResponse,
+  CurrentPaymentResponse,
+  SubscriptionData,
+  SubscriptionsRequest,
+} from '@/entities/payments/types/payments.type'
