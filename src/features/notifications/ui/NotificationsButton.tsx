afe74e183979a609a15e16a6b2d2059aa810e4d1
DePacos <depacos@yandex.ru>
2024-11-03 17:13:52 +0300

    fix: added notification

--- src/features/notifications/ui/NotificationsButton.tsx
+++ src/features/notifications/ui/NotificationsButton.tsx
@@ -1,9 +1,18 @@
+import { useState } from 'react'
+
 import { OutlineBell } from '@/common/assets/icons/components'
+import { Notifications } from '@/common/ui/notification/Notifications'
+import { notificationData } from '@/common/ui/notification/notificationsData'
 
 export const NotificationsButton = () => {
+  const [viewNotifications, setViewNotifications] = useState(false)
+
   return (
-    <button onClick={() => alert("this doesn't do anything yet")}>
-      <OutlineBell />
-    </button>
+    <>
+      <button className={'mr-[5px]'} onClick={() => setViewNotifications(!viewNotifications)}>
+        <OutlineBell />
+      </button>
+      {viewNotifications && <Notifications notificationsItems={notificationData} />}
+    </>
   )
 }
