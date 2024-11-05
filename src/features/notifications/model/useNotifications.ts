0a16688646bfbfe208013bbec10a4824f6b48057
tapuru <pogladtapira@gmail.com>
2024-11-05 12:39:22 +0500

    IN-195 refactor: change usage of useGetRelativeTime everywhere needed

--- src/features/notifications/model/useNotifications.ts
+++ src/features/notifications/model/useNotifications.ts
@@ -1,22 +1,8 @@
+import { useGetRelativeTime } from '@/common/lib/hooks/useGetRelativeTime'
 import { Notification } from '@/entities/notifications'
-import TimeAgo, { DateInput } from 'javascript-time-ago'
-import en from 'javascript-time-ago/locale/en'
-import ru from 'javascript-time-ago/locale/ru'
-import { useRouter } from 'next/router'
 
 export const useNotifications = (notificationsItems: Notification[]) => {
-  const router = useRouter()
-
-  const getRelativeTime = (date: DateInput) => {
-    TimeAgo.addDefaultLocale(en)
-    TimeAgo.addLocale(ru)
-
-    const locale = router.locale === 'en' ? 'en-US' : 'ru-RU'
-
-    const timeAgo = new TimeAgo(locale)
-
-    return timeAgo.format(date)
-  }
+  const { getRelativeTime } = useGetRelativeTime()
 
   const notificationsCount = notificationsItems.filter(notification => !notification.isRead).length
 
