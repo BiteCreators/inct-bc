4d8532b52b6aed2e99edd170d3ac5a718c631f48
DePacos <depacos@yandex.ru>
2024-11-07 19:46:31 +0300

    ref: added useEffect

--- src/features/profile/model/useLocation.ts
+++ src/features/profile/model/useLocation.ts
@@ -1,16 +1,22 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 
-import { LocationResponse } from '@/common/api/location.api'
-import { Cities, CitiesLng, CountriesLng } from '@/pages/profile/[id]/settings'
+import { Profile, ProfileResponse } from '@/common/api/profile.api'
+import { Cities, CitiesLng, CountriesLng, SelectOptions } from '@/pages/profile/[id]/settings'
 import { useRouter } from 'next/router'
 
 export const useLocation = (
   cities: CitiesLng | null,
   countries: CountriesLng | null,
-  profile: any
+  profile: (Profile & ProfileResponse) | undefined
 ) => {
   const { locale } = useRouter()
-  const [cityOptions, setCityOptions] = useState<LocationResponse[]>([])
+  const [cityOptions, setCityOptions] = useState<{ name: string; value: string }[]>([])
+
+  useEffect(() => {
+    if (profile && countries && cities) {
+      handlerCountry(profile.country)
+    }
+  }, [locale, profile])
 
   const checkCountryData = (countries: CountriesLng | null) => {
     if (countries) {
@@ -25,9 +31,9 @@ export const useLocation = (
     return []
   }
 
-  const countryOptions: LocationResponse[] = checkCountryData(countries)
+  const countryOptions: SelectOptions[] = checkCountryData(countries)
 
-  const checkError = (options: LocationResponse[]) => {
+  const checkError = (options: SelectOptions[]) => {
     if (options.length === 0 && locale === 'en') {
       return 'The list is temporarily unavailable'
     }
@@ -38,38 +44,28 @@ export const useLocation = (
     }
   }
 
-  const handlerCountry = async (countyValue: string) => {
-    setCityOptions([])
-    const getSortedCities = (cities: Cities[], countryCode: string) => {
-      // TODO delete on production!
-      if (countryCode === 'UA' || countryCode === 'RU' || countryCode === 'BY') {
-        const cityOptions = cities
-          .filter(city => city[countryCode])[0]
-          [countryCode].edges.map(city => city.node)
-          .sort((a, b) => a.name.localeCompare(b.name, locale))
+  const getSortedCities = (cities: Cities[], countryCode: string | undefined) => {
+    // TODO delete on production!
+    if (countryCode === 'UA' || countryCode === 'RU' || countryCode === 'BY') {
+      const cityOptions = cities
+        .filter(city => city[countryCode])[0]
+        [countryCode]?.sort((a, b) => a.name.localeCompare(b.name, locale))
 
-        setCityOptions(cityOptions)
-      }
-    }
-
-    if (cities && countyValue !== '') {
-      const countryCode = countryOptions.filter(country => country.name === countyValue)[0]
-        .countryCode
-
-      if (locale === 'en') {
-        getSortedCities(cities.citiesEn, countryCode)
-      }
-      if (locale === 'ru') {
-        getSortedCities(cities.citiesRu, countryCode)
-      }
+      setCityOptions(cityOptions)
     }
   }
 
-  // if (profile) {
-  //   if (profile.country.length > 0 && cityOptions.length === 0) {
-  //     handlerCountry(profile.country)
-  //   }
-  // }
+  const handlerCountry = (countyValue: string | undefined) => {
+    setCityOptions([])
+    if (cities && countries && countyValue !== '') {
+      if (locale === 'en') {
+        getSortedCities(cities.citiesEn, countyValue)
+      }
+      if (locale === 'ru') {
+        getSortedCities(cities.citiesRu, countyValue)
+      }
+    }
+  }
 
   return {
     checkError,
