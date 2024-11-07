7e81a3c5c0e4ec2af1666f7d55ef1515071dfcbe
DePacos <depacos@yandex.ru>
2024-11-07 20:33:38 +0300

    refactor

--- src/features/notifications/ui/NotificationsButton.tsx
+++ src/features/notifications/ui/NotificationsButton.tsx
@@ -1,5 +1,6 @@
 import { FillBell } from '@/common/assets/icons/components'
 import OutlineBellNoNumber from '@/common/assets/icons/components/OutlineBellNoNumber'
+import { cn } from '@/common/lib/utils/cn'
 import { Dropdown } from '@/common/ui'
 import { notificationData } from '@/common/ui/notification/notificationsData'
 import { useNotifications } from '@/features/notifications/model/useNotifications'
@@ -8,6 +9,8 @@ import { NotificationsList } from './Notifications'
 
 export const NotificationsButton = () => {
   const { notificationsCorrectDate, notificationsCount } = useNotifications(notificationData)
+  const wrapIconStyles =
+    'text-[8px] before:text-light-100 before:content-[attr(data-notificationsCount)] before:absolute before:right-0 before:block before:w-3 before:h-3 before:bg-danger-500 before:rounded-full'
 
   return (
     <div className={'relative h-6'}>
@@ -16,22 +19,12 @@ export const NotificationsButton = () => {
           'p-0 -right-[9px] top-1 [&>button]:p-0 [&>button]:text-light-100 [&>button]:hover:text-light-100'
         }
         iconButton={
-          <div
-            className={
-              'text-[8px] before:text-light-100 before:content-[attr(data-notificationsCount)] before:absolute before:right-0 before:block before:w-3 before:h-3 before:bg-danger-500 before:rounded-full'
-            }
-            data-notificationsCount={notificationsCount}
-          >
+          <div className={cn(wrapIconStyles)} data-notificationsCount={notificationsCount}>
             <OutlineBellNoNumber viewBox={'-3 2 24 24'} />
           </div>
         }
         iconButtonOpen={
-          <div
-            className={
-              'text-[8px] before:text-light-100 before:content-[attr(data-notificationsCount)] before:absolute before:right-0 before:block before:w-3 before:h-3 before:bg-danger-500 before:rounded-full'
-            }
-            data-notificationsCount={notificationsCount}
-          >
+          <div className={cn(wrapIconStyles)} data-notificationsCount={notificationsCount}>
             <FillBell className={'text-primary-500'} />
           </div>
         }
