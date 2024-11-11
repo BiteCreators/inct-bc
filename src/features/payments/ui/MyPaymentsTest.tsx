7521a543050c1825668206535f3cbfa3cd86de39
Julie <ulabalaeva07768@gmail.com>
2024-11-11 16:24:40 +0300

    IN-201:change JSX-element

--- src/features/payments/ui/MyPaymentsTest.tsx
+++ src/features/payments/ui/MyPaymentsTest.tsx
@@ -15,39 +15,82 @@ export const MyPaymentsTest = () => {
   } = useMyPaymentsTest(paymentsTotalCount)
 
   return (
-    <div className={'relative'}>
-      <table className={'w-full h-full table-auto text-sm text-light-100'}>
-        <thead>
-          <tr className={'bg-dark-500 text-left font-semibold h-[48px]'}>
-            <th className={'pl-6 pr-16'}>Date of Payment</th>
-            <th className={'pr-16'}>End date of subscription</th>
-            <th className={'pr-16'}>Price</th>
-            <th className={'pr-16'}>Subscription Type</th>
-            <th className={'pr-16'}>Payment Type</th>
-          </tr>
-        </thead>
-        <tbody className={''}>
+    <div className={'relative sm:flex sm:flex-col mb-12'}>
+      <section
+        className={
+          'w-full h-full block text-sm text-light-100 sm:table sm:table-auto sm:border-l sm:border-r sm:border-dark-500'
+        }
+      >
+        <header className={'hidden sm:table-header-group sm:w-full'}>
+          <ul className={'table-row bg-dark-500 text-left font-semibold h-12'}>
+            <li className={'pl-6 pr-16 sm:pr-[15px] table-cell align-middle'}>Date of Payment</li>
+            <li className={'pr-16 sm:pr-0 sm:mr-auto table-cell align-middle'}>
+              End date of subscription
+            </li>
+            <li className={'pr-16 sm:pr-[10px] table-cell align-middle'}>Price</li>
+            <li className={'pr-16 sm:pr-[10px] sm:box-border table-cell align-middle'}>
+              Subscription Type
+            </li>
+            <li className={'pr-16 sm:pr-[10px] sm:box-border table-cell align-middle'}>
+              Payment Type
+            </li>
+          </ul>
+        </header>
+        <div className={'flex flex-col gap-y-3 sm:table-row-group sm:w-full'}>
           {payments?.map(payment => (
-            <tr
-              className={'border-dark-500 border-b h-[48px] font-normal'}
+            <ul
+              className={
+                'sm:table-row sm:h-[48px] min-h-[200px] font-normal flex flex-col sm:bg-inherit bg-dark-500 border border-dark-300 rounded-sm'
+              }
               key={payment.subscriptionId}
             >
-              <td className={'pl-6 pr-16 border-dark-500 border-l'}>
-                {new Date(payment.endDateOfSubscription).toLocaleDateString()}
-              </td>
-              <td className={'pr-16'}>
-                {new Date(payment.endDateOfSubscription).toLocaleDateString()}
-              </td>
-              <td className={'pr-16'}>{`$${payment.price}`}</td>
-              <td className={'pr-16'}>{payment.subscriptionType}</td>
-              <td className={'pr-16 border-dark-500 border-r'}>{payment.paymentType}</td>
-            </tr>
+              <li
+                className={
+                  'flex justify-between px-[10px] py-[20px] lg:pr-[50px] sm:table-cell sm:pl-6 sm:pr-16 sm:border-dark-500 sm:border-b sm:align-middle'
+                }
+              >
+                <p className={'sm:hidden inline-block'}>Date of Payment:</p>
+                <span>{new Date(payment.endDateOfSubscription).toLocaleDateString()}</span>
+              </li>
+              <li
+                className={
+                  'flex justify-between px-[10px] py-[20px] lg:pr-[50px] sm:table-cell sm:pr-16 sm:pl-0 sm:border-b sm:border-dark-500 sm:align-middle'
+                }
+              >
+                <p className={'sm:hidden inline-block'}>End date of subscription:</p>
+                <span>{new Date(payment.endDateOfSubscription).toLocaleDateString()}</span>
+              </li>
+              <li
+                className={
+                  'flex justify-between px-[10px] py-[20px] lg:pr-[50px] sm:table-cell sm:pr-16 sm:pl-0 sm:border-b sm:border-dark-500 sm:align-middle'
+                }
+              >
+                <p className={'sm:hidden inline-block'}>Price:</p>
+                <span> {`$${payment.price}`}</span>
+              </li>
+              <li
+                className={
+                  'flex justify-between px-[10px] py-[20px] lg:pr-[50px] sm:table-cell sm:pr-16 sm:pl-0 sm:border-b sm:border-dark-500 sm:align-middle'
+                }
+              >
+                <p className={'sm:hidden inline-block'}>Subscription Type:</p>
+                <span>{payment.subscriptionType}</span>
+              </li>
+              <li
+                className={
+                  'flex justify-between px-[10px] py-[20px] lg:pr-[50px] sm:table-cell sm:pr-16 sm:pl-0 sm:border-b sm:border-dark-500 sm:align-middle'
+                }
+              >
+                <p className={'sm:hidden inline-block'}>Payment Type:</p>
+                <span>{payment.paymentType}</span>
+              </li>
+            </ul>
           ))}
-        </tbody>
-      </table>
+        </div>
+      </section>
       {isShowPagination && (
         <Pagination
-          className={'mt-9 mb-16 w-auto'}
+          className={'sm:mt-9 self-start sm:mb-16 sm:w-auto sm:inline-flex hidden'}
           currentPage={currentPage}
           onChangePagesPortion={handlePaymentsPortionChange}
           onClickPaginationButton={handleCurrentPageChange}
