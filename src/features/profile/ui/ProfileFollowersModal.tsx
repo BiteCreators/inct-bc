35a7f204b228bfcaa98fcd69ff76638564e5b56e
fireonex <mari.krulikovskaya@mail.ru>
2024-11-18 16:04:30 +0300

    IN-219 feat: add Action Confirmation for delete a Following

--- src/features/profile/ui/ProfileFollowersModal.tsx
+++ src/features/profile/ui/ProfileFollowersModal.tsx
@@ -1,4 +1,8 @@
+import React, { useState } from 'react'
+
 import { Button, Input, Modal, ScrollArea } from '@/common/ui'
+import { ActionConfirmation } from '@/common/ui/action-confirmation/ActionComfiirmation'
+import { useConfirmation } from '@/common/ui/action-confirmation/useConfirmation'
 import { FollowersResponse, followersApi } from '@/entities/followers'
 import { Follower } from '@/entities/followers/types/followers.types'
 import { UserProfile } from '@/entities/profile'
@@ -15,6 +19,11 @@ export const ProfileFollowersModal = ({ followersList }: Props) => {
   const [remove, { error: removeError, isLoading: removeLoading }] =
     followersApi.useRemoveFollowerMutation()
 
+  const { confirmOpen, handleConfirm, handleReject, requestConfirmation, setConfirmOpen } =
+    useConfirmation()
+
+  const [currentUserName, setCurrentUserName] = useState('')
+
   const handleFollow = async (userId: number) => {
     try {
       await follow({ selectedUserId: userId }).unwrap()
@@ -26,9 +35,13 @@ export const ProfileFollowersModal = ({ followersList }: Props) => {
   }
 
   const handleDeleteFollower = async (userId: number) => {
+    const confirmed = await requestConfirmation()
+
+    if (!confirmed) {
+      return
+    }
     try {
       await remove({ userId }).unwrap()
-      alert('Successfully deleted follower!')
     } catch (err) {
       //add useHandleApiError
       alert('An error occurred while trying to delete the follower.')
@@ -36,42 +49,56 @@ export const ProfileFollowersModal = ({ followersList }: Props) => {
   }
 
   return (
-    <Modal isOpen maxWidth={'max-w-[644px]'} mode={'default'} title={'2218 Followers'}>
-      <div>
-        <Input
-          className={'w-full max-w-[596px] mt-4 mb-6'}
-          inputType={'search'}
-          placeholder={'Search'}
-        />
-        <ScrollArea className={'h-[550px]'}>
-          <div className={'mr-2 mt-2'}>
-            {followersList.items.map((user: Follower) => (
-              <div className={'mb-6 flex justify-around'} key={user.userId}>
-                <UserProfile
-                  avatarUrl={user.avatars[0]?.url || example.src}
-                  className={'w-72'}
-                  profileId={user.userId}
-                  userName={user.userName}
-                />
-                <div className={'flex gap-14'}>
-                  {!user.isFollowing && (
-                    <Button disabled={followLoading} onClick={() => handleFollow(user.userId)}>
-                      Follow
+    <>
+      <ActionConfirmation
+        isOpen={confirmOpen}
+        message={`Do you really want to delete a Following ${currentUserName}?`}
+        onConfirm={handleConfirm}
+        onReject={handleReject}
+        setIsOpen={setConfirmOpen}
+        title={'Delete Following'}
+      />
+      <Modal isOpen maxWidth={'max-w-[644px]'} mode={'default'} title={'2218 Followers'}>
+        <div>
+          <Input
+            className={'w-full max-w-[596px] mt-4 mb-6'}
+            inputType={'search'}
+            placeholder={'Search'}
+          />
+          <ScrollArea className={'h-[550px]'}>
+            <div className={'mr-2 mt-2'}>
+              {followersList.items.map((user: Follower) => (
+                <div className={'mb-6 flex justify-between'} key={user.userId}>
+                  <UserProfile
+                    avatarUrl={user.avatars[0]?.url || example.src}
+                    className={'w-72'}
+                    profileId={user.userId}
+                    userName={user.userName}
+                  />
+                  <div className={'flex gap-14'}>
+                    {!user.isFollowing && (
+                      <Button disabled={followLoading} onClick={() => handleFollow(user.userId)}>
+                        Follow
+                      </Button>
+                    )}
+                    <Button
+                      className={'text-white'}
+                      disabled={removeLoading}
+                      onClick={() => {
+                        setCurrentUserName(user.userName)
+                        handleDeleteFollower(user.userId)
+                      }}
+                      variant={'text'}
+                    >
+                      Delete
                     </Button>
-                  )}
-                  <Button
-                    className={'text-white'}
-                    onClick={() => handleDeleteFollower(user.userId)}
-                    variant={'text'}
-                  >
-                    Delete
-                  </Button>
+                  </div>
                 </div>
-              </div>
-            ))}
-          </div>
-        </ScrollArea>
-      </div>
-    </Modal>
+              ))}
+            </div>
+          </ScrollArea>
+        </div>
+      </Modal>
+    </>
   )
 }
--- src/features/profile/ui/ProfileFollowingModal.tsx
+++ src/features/profile/ui/ProfileFollowingModal.tsx
@@ -1,5 +1,9 @@
+import React, { useState } from 'react'
+
 import { Button, Input, Modal, ScrollArea } from '@/common/ui'
-import { FollowersResponse } from '@/entities/followers'
+import { ActionConfirmation } from '@/common/ui/action-confirmation/ActionComfiirmation'
+import { useConfirmation } from '@/common/ui/action-confirmation/useConfirmation'
+import { FollowersResponse, followersApi } from '@/entities/followers'
 import { Follower } from '@/entities/followers/types/followers.types'
 import { UserProfile } from '@/entities/profile'
 
@@ -9,42 +13,75 @@ type Props = {
   followingList: FollowersResponse
 }
 export const ProfileFollowingModal = ({ followingList }: Props) => {
-  const handleUnfollow = () => {
-    alert(
-      'You will never be able to unfollow a user. Humble yourself. Or contact technical support'
-    )
+  const [remove, { error: removeError, isLoading: removeLoading }] =
+    followersApi.useRemoveFollowerMutation()
+
+  const { confirmOpen, handleConfirm, handleReject, requestConfirmation, setConfirmOpen } =
+    useConfirmation()
+
+  const [currentUserName, setCurrentUserName] = useState('')
+
+  const handleUnfollow = async (userId: number) => {
+    const confirmed = await requestConfirmation()
+
+    if (!confirmed) {
+      return
+    }
+    try {
+      await remove({ userId }).unwrap()
+    } catch (err) {
+      //add useHandleApiError
+      alert('An error occurred while trying to delete the follower.')
+    }
   }
 
   return (
-    <Modal isOpen maxWidth={'w-[640px]'} mode={'default'} title={'2218 Following'}>
-      <div>
-        <Input
-          className={'w-full max-w-[596px] mt-4 mb-6'}
-          inputType={'search'}
-          placeholder={'Search'}
-        />
-        <ScrollArea className={'h-[550px]'}>
-          <div className={'mr-2 mt-2'}>
-            {followingList.items.map((user: Follower) => (
-              <div className={'mb-6 flex justify-between'} key={user.userId}>
-                <UserProfile
-                  avatarUrl={user.avatars[0]?.url || example.src}
-                  className={'w-72'}
-                  profileId={user.userId}
-                  userName={user.userName}
-                />
-                {/*isFollowedBy это подписан ли юзер на текущего пользователя*/}
-                {/*isFollowing это подписан ли текущий пользователь на юзера*/}
-                {/*Following это те на которых я подписан*/}
-                {/*Followers это мои подписчики*/}
-                <Button onClick={handleUnfollow} variant={'outline'}>
-                  Unfollow
-                </Button>
-              </div>
-            ))}
-          </div>
-        </ScrollArea>
-      </div>
-    </Modal>
+    <>
+      <ActionConfirmation
+        isOpen={confirmOpen}
+        message={`Do you really want to delete a Following ${currentUserName}?`}
+        onConfirm={handleConfirm}
+        onReject={handleReject}
+        setIsOpen={setConfirmOpen}
+        title={'Delete Following'}
+      />
+      <Modal isOpen maxWidth={'w-[640px]'} mode={'default'} title={'2218 Following'}>
+        <div>
+          <Input
+            className={'w-full max-w-[596px] mt-4 mb-6'}
+            inputType={'search'}
+            placeholder={'Search'}
+          />
+          <ScrollArea className={'h-[550px]'}>
+            <div className={'mr-2 mt-2'}>
+              {followingList.items.map((user: Follower) => (
+                <div className={'mb-6 flex justify-between'} key={user.userId}>
+                  <UserProfile
+                    avatarUrl={user.avatars[0]?.url || example.src}
+                    className={'w-72'}
+                    profileId={user.userId}
+                    userName={user.userName}
+                  />
+                  {/*isFollowedBy это подписан ли юзер на текущего пользователя*/}
+                  {/*isFollowing это подписан ли текущий пользователь на юзера*/}
+                  {/*Following это те на которых я подписан*/}
+                  {/*Followers это мои подписчики*/}
+                  <Button
+                    disabled={removeLoading}
+                    onClick={() => {
+                      setCurrentUserName(user.userName)
+                      handleUnfollow(user.userId)
+                    }}
+                    variant={'outline'}
+                  >
+                    Unfollow
+                  </Button>
+                </div>
+              ))}
+            </div>
+          </ScrollArea>
+        </div>
+      </Modal>
+    </>
   )
 }
