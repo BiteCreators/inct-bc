879379cb540d7482c43647553a70db51a6447f73
DePacos <depacos@yandex.ru>
2024-11-21 23:57:25 +0300

    ref: msw-storybook-addon setting

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,5 +1,10 @@
 import type { Preview } from '@storybook/react'
+import { initialize, mswLoader } from 'msw-storybook-addon'
 import '../src/app/styles/globals.css'
+
+initialize({
+  onUnhandledRequest: 'bypass',
+})
 
 const preview: Preview = {
   parameters: {
@@ -10,6 +15,7 @@ const preview: Preview = {
       },
     },
   },
+  loaders: [mswLoader],
 }
 
 export default preview
