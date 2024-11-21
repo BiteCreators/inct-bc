9f92c5633ea9dd20f08f31cbe3f0836df0245f44
DePacos <depacos@yandex.ru>
2024-11-21 23:49:20 +0300

    ref: remove observer

--- src/features/notifications/model/useNotifications.ts
+++ src/features/notifications/model/useNotifications.ts
@@ -11,55 +11,65 @@ export const useNotifications = ({
   const { getRelativeTime } = useGetRelativeTime()
   const [markAsRead] = notificationsApi.useMarkAsReadMutation()
 
-  const [readIds, setReadIds] = useState<number[]>([])
-  const notificationRefs = useRef<(HTMLDivElement | null)[]>([])
+  // const [readIds, setReadIds] = useState<number[]>([])
+  // const notificationRefs = useRef<(HTMLDivElement | null)[]>([])
 
   const notificationsCorrectDate = notificationsItems?.map(notification => {
     return { ...notification, notifyAt: getRelativeTime(new Date(notification.notifyAt).getTime()) }
   })
 
+  const notificationsIds = notificationsItems!
+    .filter(notifications => !notifications.isRead)
+    .map(n => n.id)
+
   useEffect(() => {
-    const handleMarkAsRead = async (id: number) => {
-      await markAsRead({ ids: [id] }).unwrap()
-      console.log('Прочитано:', id)
-    }
-    const observer = new IntersectionObserver(
-      entries => {
-        entries.forEach(entry => {
-          if (entry.isIntersecting) {
-            const notificationId = Number(entry.target.getAttribute('data-id'))
-
-            if (!readIds.includes(notificationId)) {
-              setReadIds(prevIds => [...prevIds, notificationId])
-              handleMarkAsRead(notificationId)
-            }
-          }
-        })
-      },
-      {
-        threshold: 1.0,
-      }
-    )
-
-    const currentRefs = notificationRefs.current
-
-    currentRefs.forEach(ref => {
-      if (ref) {
-        observer.observe(ref)
-      }
-    })
-
     return () => {
-      currentRefs.forEach(ref => {
-        if (ref) {
-          observer.unobserve(ref)
-        }
-      })
+      markAsRead({ ids: notificationsIds })
     }
-  }, [notificationsItems, readIds, markAsRead])
+  }, [])
+
+  // useEffect(() => {
+  //   const handleMarkAsRead = async (id: number) => {
+  //     await markAsRead({ ids: [id] }).unwrap()
+  //     console.log('Прочитано:', id)
+  //   }
+  //   const observer = new IntersectionObserver(
+  //     entries => {
+  //       entries.forEach(entry => {
+  //         if (entry.isIntersecting) {
+  //           const notificationId = Number(entry.target.getAttribute('data-id'))
+  //
+  //           if (!readIds.includes(notificationId)) {
+  //             setReadIds(prevIds => [...prevIds, notificationId])
+  //             handleMarkAsRead(notificationId)
+  //           }
+  //         }
+  //       })
+  //     },
+  //     {
+  //       threshold: 1.0,
+  //     }
+  //   )
+  //
+  //   const currentRefs = notificationRefs.current
+  //
+  //   currentRefs.forEach(ref => {
+  //     if (ref) {
+  //       observer.observe(ref)
+  //     }
+  //   })
+  //
+  //   return () => {
+  //     currentRefs.forEach(ref => {
+  //       if (ref) {
+  //         observer.unobserve(ref)
+  //       }
+  //     })
+  //   }
+  // }, [notificationsItems, readIds, markAsRead])
 
   return {
-    notificationRefs,
+    // notificationRefs,
     notificationsCorrectDate,
   }
 }
