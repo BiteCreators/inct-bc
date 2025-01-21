c6ff6e34eab5edf02396bf29282e73aaa0f61342
tapuru <pogladtapira@gmail.com>
2025-01-21 23:44:22 +0500

    IN-333 feat: make rtk query work with ssr

--- src/common/api/inct.api.ts
+++ src/common/api/inct.api.ts
@@ -1,9 +1,17 @@
 import { baseQueryWithReauth } from '@/common/api/base-query'
 import { createApi } from '@reduxjs/toolkit/query/react'
+import { HYDRATE } from 'next-redux-wrapper'
+
+import { isHydrateAction } from '../lib/utils/isHydrateAction'
 
 export const inctagramApi = createApi({
   baseQuery: baseQueryWithReauth,
   endpoints: () => ({}),
+  extractRehydrationInfo(action, { reducerPath }): any {
+    if (isHydrateAction(action)) {
+      return action.payload[reducerPath]
+    }
+  },
   reducerPath: 'inctagramApi',
   tagTypes: ['Me', 'Profile', 'Devices', 'Post', 'Followers', 'Comment', 'Answer'],
 })
