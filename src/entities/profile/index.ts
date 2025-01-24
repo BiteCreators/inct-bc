e65ca976f4ae3268cc1e30351fbf81a92d3b3f35
DmitryChues <dmitrychues2312@gmail.com>
2025-01-24 19:19:11 +0300

    IN-323: used the user profile component from ui-kit

--- src/entities/profile/index.ts
+++ src/entities/profile/index.ts
@@ -1,4 +1,3 @@
 export { profileApi } from './api/profile.api'
 export { profileHanlders } from './api/profile.mocks'
 export type { Profile } from './types/profile.type'
-export { UserProfile } from './ui/UserProfile'
