741c309a42d123391086c01a0cc011e27c3d2116
DePacos <depacos@yandex.ru>
2025-01-29 19:33:01 +0300

    feat: added ssr

--- src/widgets/post-details/ui/PostDetails.tsx
+++ src/widgets/post-details/ui/PostDetails.tsx
@@ -1,39 +1,46 @@
 import { commentsApi } from '@/entities/comments'
-import { Post } from '@/entities/posts'
+import { postsApi } from '@/entities/posts'
+import { PostMobile } from '@/widgets/post-details/ui/mobile/PostMobile'
 import { useMediaQuery } from '@byte-creators/utils'
+import { skipToken } from '@reduxjs/toolkit/query'
+import { useParams } from 'next/navigation'
 import { useRouter } from 'next/router'
 
 import { PostDetailsSlide } from './PostDetailsSlide'
 import { PostDesktop } from './desktop/PostDesktop'
-import { PostMobile } from './mobile/PostMobile'
 
-type Props = {
-  post: Post
-}
+export const PostDetails = () => {
+  const params = useParams()
 
-export const PostDetails = ({ post }: Props) => {
+  const { data: post } = postsApi.useGetPublicPostByIdQuery(
+    params !== null ? { postId: Number(params.postId) } : skipToken
+  )
+
   const router = useRouter()
+
   const handleNavigateToImage = (imageUrl: string) => {
     const proxyUrl = `/api/proxy?path=${encodeURIComponent(imageUrl)}`
 
     router.push({
-      pathname: `/profile/${post.ownerId}/publications/${post.id}/view`,
+      pathname: `/profile/${post?.ownerId}/publications/${post?.id}/view`,
       query: { image: proxyUrl },
     })
   }
-  const slides = post.images.map((image: any, i) => (
+  const slides = post?.images.map((image: any, i) => (
     <PostDetailsSlide handleNavigateToImage={handleNavigateToImage} image={image} key={i} />
   ))
 
   const isLargeScreen = useMediaQuery('(min-width: 768px)')
 
-  const { data, error, isLoading } = commentsApi.useGetCommentsQuery({ postId: post.id })
+  const { data, error, isLoading } = commentsApi.useGetCommentsQuery({ postId: post?.id || 0 })
 
   const comments = data?.items
 
   if (isLargeScreen) {
-    return <PostDesktop comments={comments} isLoading={isLoading} post={post} slides={slides} />
+    return (
+      <PostDesktop comments={comments} isLoading={isLoading} post={post} slides={slides || []} />
+    )
   } else {
-    return <PostMobile comments={comments} post={post} slides={slides} />
+    return <PostMobile comments={comments} post={post} slides={slides || []} />
   }
 }
