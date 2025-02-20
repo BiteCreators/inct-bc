b6ac22d3d8d03804f3d1eed83a1559310fa04bda
tapuru <pogladtapira@gmail.com>
2025-02-20 21:13:57 +0500

    IN-335 remove state for followers numbers

--- src/features/profile/model/useProfileFollow.ts
+++ src/features/profile/model/useProfileFollow.ts
@@ -1,6 +1,5 @@
-import { useEffect, useState } from 'react'
+import { useState } from 'react'
 
-import { useAppDispatch } from '@/common/lib/hooks/reduxHooks'
 import { authApi } from '@/entities/auth'
 import { followersApi } from '@/entities/followers'
 import { Follower, UserProfile } from '@/entities/followers/types/followers.types'
@@ -15,19 +14,9 @@ export const useProfileFollow = (currentUserProfile: UserProfile) => {
     userName: currentUserProfile.userName,
   })
 
-  const [following, setFollowing] = useState<number>(0)
-  const [followers, setFollowers] = useState<number>(0)
+  const following = followingList?.items.length
+  const followers = followersList?.items.length
 
-  useEffect(() => {
-    if (followingList?.items) {
-      setFollowing(followingList.items.length)
-    }
-    if (followersList?.items) {
-      setFollowers(followersList.items.length)
-    }
-  }, [followingList, followersList])
-
-  const dispatch = useAppDispatch()
   const { data: me } = authApi.useMeQuery()
   const [follow, { isLoading: followLoading }] = followersApi.useFollowMutation()
 
