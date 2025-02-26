aa346f47be5cd94e1a61c112927bb0542e5bb172
fireonex <mari.krulikovskaya@mail.ru>
2025-02-26 15:03:14 +0300

    IN-380 init: init profile page adaptive task

--- src/features/posts/ui/Posts.tsx
+++ src/features/posts/ui/Posts.tsx
@@ -3,7 +3,7 @@ import Skeleton from 'react-loading-skeleton'
 
 import { postsApi } from '@/entities/posts'
 import { Typography } from '@byte-creators/ui-kit'
-import { useIntersectionObserver } from '@byte-creators/utils'
+import { useIntersectionObserver, useMediaQuery } from '@byte-creators/utils'
 import { skipToken } from '@reduxjs/toolkit/query'
 import Link from 'next/link'
 import { useParams } from 'next/navigation'
@@ -12,6 +12,7 @@ export const Posts = () => {
   const [pageSize, setPageSize] = useState(8)
   const params = useParams<{ id: string }>()
   const paginationRef = useRef<HTMLDivElement>(null)
+  const isLargeScreen = useMediaQuery('(min-width: 768px)')
 
   const { data, isFetching, isLoading } = postsApi.useGetPublicPostsByUserIdQuery(
     params !== null
@@ -29,30 +30,44 @@ export const Posts = () => {
       setPageSize(prev => prev + 8)
     }
   })
-
+  //todo: fix skeleton size
   const skeletonItems = Array.from({ length: 5 }, (_, index) => (
-    <Skeleton className={'!w-64 !h-64 rounded-md'} key={index} />
+    <Skeleton
+      className={`rounded-md ${isLargeScreen ? '!w-[260px] !h-[260px]' : '!w-[108px] !h-[108px]'}`}
+      key={index}
+    />
   ))
 
   return (
-    <div className={'flex gap-5 justify-center flex-wrap relative'}>
+    <div className={'my-7 md:my-2'}>
       {!isLoading && data?.items && data?.items.length < 1 ? (
         <Typography> user has no publications yet </Typography>
       ) : (
-        <>
+        <div
+          className={`grid gap-4 ${
+            isLargeScreen ? 'grid-cols-4' : 'grid-cols-2'
+          } sm:grid-cols-3 md:grid-cols-4`}
+        >
           {data?.items.map(post => (
             <Link
               className={'hover:scale-[1.013] duration-75'}
               href={`/profile/${params.id}/publications/${post.id}`}
               key={post.id}
             >
-              <img alt={'post-img'} height={260} src={post.images[0]?.url} width={260} />
+              <img
+                alt={'post-img'}
+                className={'w-full h-auto rounded-md'}
+                height={isLargeScreen ? 260 : 108}
+                src={post.images[0]?.url}
+                width={isLargeScreen ? 260 : 108}
+              />
             </Link>
           ))}
+          {/* Добавляем скелетоны в сетку */}
+          {(isFetching || isLoading) && skeletonItems}
           <div ref={paginationRef}></div>
-        </>
+        </div>
       )}
-      {(isFetching || isLoading) && skeletonItems}
     </div>
   )
 }
