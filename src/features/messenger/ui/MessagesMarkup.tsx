def7314e9f2d9409c2720bb38e5d67147b5a4eb9
fireonex <mari.krulikovskaya@mail.ru>
2025-03-04 14:47:31 +0300

    IN-391 refactor: add Message component

--- src/features/messenger/ui/MessagesMarkup.tsx
+++ src/features/messenger/ui/MessagesMarkup.tsx
@@ -1,8 +1,6 @@
 import { mockData, mockImages, step } from '@/features/messenger/mockData'
-import { UserAvatar } from '@/widgets/profile-header/ui/UserAvatar'
-import { ScrollArea, Typography } from '@byte-creators/ui-kit'
-import { CheckmarkOutline, DoneAllOutline } from '@byte-creators/ui-kit/icons'
-import { cn } from '@byte-creators/utils'
+import { Message } from '@/features/messenger/ui/Message'
+import { ScrollArea } from '@byte-creators/ui-kit'
 
 type Props = {}
 export const MessagesMarkup = ({}: Props) => {
@@ -18,54 +16,16 @@ export const MessagesMarkup = ({}: Props) => {
           const isReadMessage = item.status === 'READ' && isOwner
 
           return (
-            <div className={cn(['flex my-6 relative'], isOwner && 'justify-end')} key={item.id}>
-              <UserAvatar
-                className={cn([
-                  'w-9 absolute bottom-[18px] transform translate-y-1/2',
-                  isOwner ? '-right-12' : '-left-12',
-                ])}
-                isLoading={false}
-                src={''}
-              />
-              <div
-                className={cn(
-                  ['max-w-80 h-fit rounded-lg  flex flex-col'],
-                  isOwner ? 'bg-primary-900' : 'bg-dark-300',
-                  imgMessageWithoutText && 'bg-transparent'
-                )}
-              >
-                {imgMessage && (
-                  <img
-                    alt={'Image message'}
-                    className={'object-contain rounded-sm'}
-                    src={item.url}
-                  />
-                )}
-                {!imgMessageWithoutText ? (
-                  <Typography className={'px-3 mt-2'} variant={'regular-text'}>
-                    {voiceMessage ? 'Voice message' : item.messageText}
-                  </Typography>
-                ) : null}
-
-                <Typography
-                  className={cn([
-                    'flex pb-2 text-light-900 self-end mt-1',
-                    !imgMessageWithoutText && 'pr-3',
-                  ])}
-                  variant={'small-text'}
-                >
-                  {new Date(item.createdAt).toLocaleTimeString('en-US', {
-                    hour: '2-digit',
-                    hour12: false,
-                    minute: '2-digit',
-                  })}
-                  {isReceivedMessage && (
-                    <CheckmarkOutline height={16} viewBox={'0 0 20 25'} width={16} />
-                  )}
-                  {isReadMessage && <DoneAllOutline height={16} viewBox={'0 0 20 25'} width={16} />}
-                </Typography>
-              </div>
-            </div>
+            <Message
+              imgMessage={imgMessage}
+              imgMessageWithoutText={imgMessageWithoutText}
+              isOwner={isOwner}
+              isReadMessage={isReadMessage}
+              isReceivedMessage={isReceivedMessage}
+              item={item}
+              key={item.id}
+              voiceMessage={voiceMessage}
+            />
           )
         })}
       </div>
