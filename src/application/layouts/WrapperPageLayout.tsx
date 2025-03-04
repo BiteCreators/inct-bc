f140dc062dbd620c81a31bc97770e0e82969f001
fireonex <mari.krulikovskaya@mail.ru>
2025-03-04 15:26:03 +0300

    fix: fix sidebar display condition in WrapperPageLayout

--- src/application/layouts/WrapperPageLayout.tsx
+++ src/application/layouts/WrapperPageLayout.tsx
@@ -6,15 +6,18 @@ import { MobileAppMenu } from '@/features/navigation'
 import { Header } from '@/widgets/header'
 import { Sidebar } from '@/widgets/sidebar'
 import { PageLayout } from '@byte-creators/ui-kit'
+import { useRouter } from 'next/router'
 
 export const WrapperPageLayout = ({ children }: { children: ReactNode }) => {
   const userId = useAppSelector(selectUserId)
+  const router = useRouter()
+  const isViewPostPage = router.pathname.includes('/view')
 
   return (
     <PageLayout
       header={<Header />}
       mobileMenu={<MobileAppMenu />}
-      sidebar={userId ? <Sidebar /> : null}
+      sidebar={userId && !isViewPostPage ? <Sidebar /> : null}
     >
       {children}
     </PageLayout>
