f4a26bf00955aa26dc9e86a29a5e2a9d2b26414a
fireonex <mari.krulikovskaya@mail.ru>
2025-04-01 14:59:07 +0300

    IN-407 feat: add error output about cookie restrictions

--- src/features/auth/model/useSignInForm.ts
+++ src/features/auth/model/useSignInForm.ts
@@ -8,14 +8,12 @@ import { authApi, authSlice, decodeAccessToken } from '@/entities/auth'
 import { SignInFormData, createSignInSchema } from '@/features/auth/lib/schemas/signIn.schema'
 import { useScopedTranslation } from '@byte-creators/utils'
 import { zodResolver } from '@hookform/resolvers/zod'
-import * as jose from 'jose'
 import Router from 'next/router'
 
 import { modifySingInApiError } from '../lib/modifySignInApiError'
 
 export const useSignInForm = () => {
   const t = useScopedTranslation('Auth')
-
   const signInSchema = createSignInSchema(t.errors)
 
   const {
@@ -31,6 +29,7 @@ export const useSignInForm = () => {
     mode: 'onChange',
     resolver: zodResolver(signInSchema),
   })
+
   const [login, { isLoading }] = authApi.useLoginMutation()
   const dispatch = useAppDispatch()
   const { handleApiError } = useHandleApiError('Auth')
@@ -44,6 +43,7 @@ export const useSignInForm = () => {
         email,
         password,
       }).unwrap()
+
       const token = res.accessToken
       const { userId } = decodeAccessToken(token)
 
@@ -51,21 +51,34 @@ export const useSignInForm = () => {
         throw new Error('access token is invalid')
       }
 
-      setCookies('accessToken', res.accessToken, {
+      setCookies('accessToken', token, {
         maxAge: 2678400,
         path: '/',
         sameSite: 'lax',
         secure: true,
       })
+
+      await new Promise(resolve => setTimeout(resolve, 500))
+
+      if (!document.cookie.includes('accessToken')) {
+        throw new Error('COOKIES_BLOCKED')
+      }
+
       dispatch(authSlice.actions.setCredentials({ accessToken: token, userId }))
       Router.push(`/profile/${userId}`)
-    } catch (error) {
-      handleApiError({
-        error,
-        modifyMessage: modifySingInApiError,
-        setApiError,
-        setError,
-      })
+    } catch (error: any) {
+      if (error.message === 'COOKIES_BLOCKED') {
+        setApiError(
+          'Для входа на сайт необходимо разрешить использование файлов cookie в настройках вашего браузера.'
+        )
+      } else {
+        handleApiError({
+          error,
+          modifyMessage: modifySingInApiError,
+          setApiError,
+          setError,
+        })
+      }
     }
   }
 
