ea4acbc7e0d5825dbf62a20c1ec7c158f8493abb
Dmitriy <fstpain@gmail.com>
2025-04-03 17:26:22 +0300

    IN-405:messenger+host

--- src/pages/messenger/Messenger.tsx
+++ src/pages/messenger/Messenger.tsx
@@ -1,5 +1,12 @@
+import dynamic from 'next/dynamic'
+
+// eslint-disable-next-line import/no-unresolved
+export const RemoteMessenger = dynamic(() => import('messenger/Messenger'), {
+  ssr: false,
+})
+
 const Messenger = () => {
-  return <div>messenger</div>
+  return <RemoteMessenger />
 }
 
 export default Messenger
